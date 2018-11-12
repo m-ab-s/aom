926e3bdc49b9a224545190aa3d3255a50bb9df23
Frederic Barbier <frederic.barbier@allegrodvt.com>
2018-11-12 09:42:12 -0800

    Move aom_shift_img() to tools_common
    cherry-pick of 3a607f7b00e9e9a47675c510c4196f792a6d3a28 from master
    
    Moved aom_shift_img() to tools_common, so that it can be called from
    other files.
    
    BUG=aomedia:1999
    
    Change-Id: I3a4fec1056fb2222573a6887883ff92bea0df4dc

--- common/tools_common.h
+++ common/tools_common.h
@@ -154,6 +154,8 @@ int aom_img_read(aom_image_t *img, FILE *file);
 double sse_to_psnr(double samples, double peak, double mse);
 void aom_img_upshift(aom_image_t *dst, aom_image_t *src, int input_shift);
 void aom_img_downshift(aom_image_t *dst, aom_image_t *src, int down_shift);
+void aom_shift_img(unsigned int output_bit_depth, aom_image_t **img_ptr,
+                   aom_image_t **img_shifted_ptr);
 void aom_img_truncate_16_to_8(aom_image_t *dst, aom_image_t *src);
 
 #ifdef __cplusplus
