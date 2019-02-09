494480807b3b024670ee03c42acba6a89c53b479
Debargha Mukherjee <debargha@google.com>
2019-02-09 11:27:55 -0800

    Add expt macro for CNN based in-loop restoration
    Change-Id: I1bf4536556421dd2cfadc68016b3998f959f91ee

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -134,11 +134,15 @@ set_aom_config_var(CONFIG_INTER_STATS_ONLY 0 NUMBER "AV1 experiment flag.")
 set_aom_config_var(CONFIG_RD_DEBUG 0 NUMBER "AV1 experiment flag.")
 set_aom_config_var(CONFIG_2PASS_PARTITION_SEARCH_LVL 1 NUMBER
                    "AV1 experiment flag.")
-set_aom_config_var(CONFIG_ADAPT_FILTER_INTRA 0 NUMBER "AV1 experiment flag.")
 set_aom_config_var(CONFIG_SHARP_SETTINGS 0 NUMBER "AV1 experiment flag.")
 set_aom_config_var(CONFIG_ONE_PASS_SVM 0 NUMBER "AV1 experiment flag.")
 set_aom_config_var(CONFIG_DISABLE_FULL_PIXEL_SPLIT_8X8 1 NUMBER
                    "Disable full_pixel_motion_search_based_split on BLOCK_8X8")
+
+# AV2 experiment flags
+set_aom_config_var(CONFIG_ADAPT_FILTER_INTRA 0 NUMBER "AV1 experiment flag.")
+set_aom_config_var(CONFIG_CNN_RESTORATION 0 NUMBER
+	           "CNN Restoration experiment flag")
 
 #
 # Variables in this section control optional features of the build system.
