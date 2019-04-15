b22520d4f979f4d9bab24ffc0e38869b305b2ea5
logangw <logangw@google.com>
2019-04-15 12:41:17 -0700

    Add script to translate from ckpt to c-header.
    Change-Id: I8b7da7f82fea7bace937e4ae927fd67c293a8bad

---
+++ tools/tf_ckpt_to_c_header.py
@@ -0,0 +1,328 @@
+# Copyright (c) 2019, Alliance for Open Media. All rights reserved
+#
+# This source code is subject to the terms of the BSD 2 Clause License and
+# the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+# was not distributed with this source code in the LICENSE file, you can
+# obtain it at www.aomedia.org/license/software. If the Alliance for Open
+# Media Patent License 1.0 was not distributed with this source code in the
+# PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+#
+
+r"""Python module that transforms a Tensorflow ckpt to a C header file.
+
+Usage: tf_ckpt_to_c_header.py [-h] [--input_path INPUT_PATH]
+                              [--output_path OUTPUT_PATH]
+                              [--header_guard HEADER_GUARD]
+                              [--config_name CONFIG_NAME]
+                              [--var_regex VAR_REGEX]
+                              [--trained_qp TRAINED_QP]
+                              [--is_residue IS_RESIDUE]
+                              [--ext_width EXT_WIDTH]
+                              [--ext_height EXT_HEIGHT]
+                              [--strict_bounds STRICT_BOUNDS]
+                              [--enable_explicit_field_names]
+
+Optional Arguments:
+  -h, --help            show this help message and exit
+  --input_path INPUT_PATH
+                        Path to ckpt. Please include the full prefix of all
+                        relevant ckpt files.
+  --output_path OUTPUT_PATH
+                        Path to output of file.
+  --header_guard HEADER_GUARD
+                        Name of the header file header guard.
+  --config_name CONFIG_NAME
+                        Name of model and prefix to all relevant weight, bias,
+                        and qp variables.
+  --var_regex VAR_REGEX
+                        Regex to match tensor names against in the model ckpt.
+  --trained_qp TRAINED_QP
+                        The primary QP of the model.
+  --is_residue IS_RESIDUE
+                        Whether we are predicting an image or the residue of
+                        an image.
+  --ext_width EXT_WIDTH
+                        Width of the frame extension.
+  --ext_height EXT_HEIGHT
+                        Height of the frame extension.
+  --strict_bounds STRICT_BOUNDS
+                        Whether the input bounds are strict or not.
+  --enable_explicit_field_names
+                        Whether to print field names along side values in cnn
+                        config.
+
+Example Invocation:
+$ python tf_ckpt_to_c_header.py --input_path="./model.ckpt" --output_path=
+    "./output.h" --header_guard="MODEL_H_" --trained_qp=32
+    --enable_explicit_field_names
+"""
+
+import argparse
+import collections
+import re
+import sys
+
+from tensorflow import logging
+from tensorflow.python import pywrap_tensorflow
+from tensorflow.python.platform import app
+
+FLAGS = None
+CNN_CONFIG_ORDER = {}
+LAYER_CONFIG_ORDER = {}
+
+
+def _generate_layer_index_tensor_name_map(input_reader, var_regex):
+  """Create a map from tuples of indices to tensor names."""
+  layer_index_tensor_name_map = {}
+  # Create a index-tuple to tensor name map.
+  for k, _ in input_reader.get_variable_to_shape_map().iteritems():
+    match = re.match(var_regex, k)
+    if match:
+      var_indices = tuple(int(x) for x in match.group(1).split("_")[:-1])
+      # Value is the prefix of the layer variables.
+      layer_index_tensor_name_map[var_indices] = k[:-1]
+  return layer_index_tensor_name_map
+
+
+def _format_tensor(tensor, layer):
+  """Reformats the tensor from Python-style array to C-style array."""
+  if isinstance(tensor, basestring):
+    parsed_tensor = re.sub(
+        r"[\[\]]", "",
+        re.sub(r"[\s\s+ ]+", " ", re.sub(r"(-*[0-9]*[.,][0-9]*)", r"\1f,",
+                                         tensor))).strip()
+    if not sum([float(x) for x in parsed_tensor.split("f,") if x]):
+      logging.warning("Tensor at layer %d is a zero tensor!", layer)
+  else:
+    flattened_tensor = tensor.flatten()
+    if not sum(flattened_tensor):
+      logging.warning("Tensor at layer %d is a zero tensor!", layer)
+    parsed_tensor = " ".join([("%ff," % value) for value in flattened_tensor])
+  return "{" + parsed_tensor + "}"
+
+
+def _extract_layer_variables(input_reader):
+  """Extracts the graph, weights, and biases from input_file."""
+  cnn_config = CNN_CONFIG_ORDER
+  layer_index_tensor_name_map = _generate_layer_index_tensor_name_map(
+      input_reader, FLAGS.var_regex)
+  num_layers = len(layer_index_tensor_name_map)
+  cnn_config["num_layers"] = num_layers
+
+  cnn_config["layer_config"] = [None] * num_layers
+  weights = [None] * num_layers
+  biases = [None] * num_layers
+
+  layer = 0
+  # By the end of this process, every layer should have weights and biases.
+  for _, tensor_name in sorted(layer_index_tensor_name_map.iteritems()):
+    if cnn_config["layer_config"][layer]:
+      layer_config = cnn_config["layer_config"][layer]
+    else:
+      layer_config = LAYER_CONFIG_ORDER
+      layer_config["weights"] = "%s_weight_%s" % (FLAGS.config_name, layer)
+      shape = input_reader.get_variable_to_shape_map()[tensor_name + "w"]
+      layer_config["filter_width"] = shape[0]
+      layer_config["filter_height"] = shape[1]
+      layer_config["in_channels"] = shape[2]
+      layer_config["out_channels"] = shape[3]
+      weights[layer] = _format_tensor(
+          input_reader.get_tensor(tensor_name + "w"), layer)
+      layer_config["bias"] = "%s_bias_%s" % (FLAGS.config_name, layer)
+      biases[layer] = _format_tensor(
+          input_reader.get_tensor(tensor_name + "b"), layer)
+    cnn_config["layer_config"][layer] = layer_config
+    layer += 1
+  return cnn_config, weights, biases
+
+
+def _print_header(header_guard, output_file):
+  """Writes the header of the header file. Contains the aom license."""
+  output_file.write("""/*
+ * Copyright (c) 2019, Alliance for Open Media. All rights reserved
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+""")
+  output_file.write("\n#ifndef %s" % header_guard)
+  output_file.write("\n#define %s\n" % header_guard)
+  output_file.write("\n#include \"av1/common/cnn.h\"\n")
+
+
+def _print_cnn_config(cnn_config, weights, biases, output_file):
+  """Write the contents of the cnn to the header file.
+
+  Each variable has static const prepended to them to ensure they remain
+  local to the header and any file included them and need not be modified.
+  All variables have FLAGS.config_name prepended to their name to distinguish
+  them from other variables in other model header files.
+
+  The layout of the file is as follows:
+    - qp of the model,
+    - weights and biases in alternating order,
+    - the graph.
+
+  The variables printed in the graph are determined by CNN_CONFIG_ORDER
+  and LAYER_CONFIG_ORDER. It is especially important to maintain the orders in
+  each of these constants because C expects that any initialized struct
+  maintains the order of the fields it has in its declaration.
+
+  Args:
+    cnn_config: A dictionary containing all the graph parameters.
+    weights: An array containing the weights of each layer in order.
+    biases: An array containing the bias of each layer in order.
+    output_file: The output file.
+  """
+  output_file.write("\nstatic int %s_trained_qp = %d;\n" %
+                    (FLAGS.config_name, FLAGS.trained_qp))
+  for layer in range(cnn_config["num_layers"]):
+    output_file.write("\nstatic float %s_weight_%d[] = %s;\n" %
+                      (FLAGS.config_name, layer, weights[layer]))
+    output_file.write("\nstatic float %s_bias_%d[] = %s;\n" %
+                      (FLAGS.config_name, layer, biases[layer]))
+
+  output_file.write("\nconst CNN_CONFIG %s = {" % FLAGS.config_name)
+  for key in CNN_CONFIG_ORDER:
+    if FLAGS.enable_explicit_field_names:
+      output_file.write("\n  .%s = %s," % (key, cnn_config[key]))
+    else:
+      output_file.write("\n  %s," % cnn_config[key])
+  output_file.write("\n  .layer_config = {")
+  for layer_num, layer in enumerate(cnn_config["layer_config"]):
+    output_file.write("\n    { // layer_%d" % layer_num)
+    for key in LAYER_CONFIG_ORDER:
+      if FLAGS.enable_explicit_field_names:
+        output_file.write("\n  .%s = %s," % (key, layer[key]))
+      else:
+        output_file.write("\n  %s," % layer[key])
+    output_file.write("\n    },")
+  output_file.write("\n  }")
+  output_file.write("\n};\n")
+
+
+def _print_footer(header_guard, output_file):
+  """Writes the footer of the header file."""
+  output_file.write("\n#endif  // %s" % header_guard)
+
+
+def generate_header_file():
+  """Generates a C header file from a Tensorflow model ckpt."""
+  try:
+    input_reader = pywrap_tensorflow.NewCheckpointReader(FLAGS.input_path)
+  except Exception as e:
+    raise e
+  cnn_config, weights, biases = _extract_layer_variables(input_reader)
+
+  assert not [weight for weight in weights if weight is None]
+  assert not [bias for bias in biases if bias is None]
+  assert not [
+      config for config in cnn_config["layer_config"] if config is None
+  ]
+
+  with open(FLAGS.output_path, "w+") as output_file:
+    _print_header(FLAGS.header_guard, output_file)
+    _print_cnn_config(cnn_config, weights, biases, output_file)
+    _print_footer(FLAGS.header_guard, output_file)
+
+    output_file.close()
+
+
+def main(_):
+  generate_header_file()
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+      "--input_path",
+      type=str,
+      default="",
+      help="Path to ckpt. Please include the full prefix of all relevant ckpt "
+      " files.")
+  parser.add_argument(
+      "--output_path",
+      type=str,
+      default="",
+      help="Path to output of file.")
+  parser.add_argument(
+      "--header_guard",
+      type=str,
+      default="AOM_AV1_COMMON_MODEL_H_",
+      help="Name of the header file header guard.")
+  parser.add_argument(
+      "--config_name",
+      type=str,
+      default="model",
+      help="Name of model and prefix to all relevant weight, bias, and qp "
+      "variables.")
+  parser.add_argument(
+      "--var_regex",
+      type=str,
+      default=r"conv_(([0-9][0-9]*_)*)(w|b)",
+      help="Regex to match tensor names against in the model ckpt.")
+  parser.add_argument(
+      "--trained_qp",
+      type=int,
+      default=32,
+      help="The primary QP of the model.")
+  parser.add_argument(
+      "--is_residue",
+      type=int,
+      default=0,
+      help="Whether we are predicting an image or the residue of an image.")
+  parser.add_argument(
+      "--ext_width",
+      type=int,
+      default=0,
+      help="Width of the frame extension.")
+  parser.add_argument(
+      "--ext_height",
+      type=int,
+      default=0,
+      help="Height of the frame extension.")
+  parser.add_argument(
+      "--strict_bounds",
+      type=int,
+      default=0,
+      help="Whether the input bounds are strict or not.")
+  parser.add_argument(
+      "--enable_explicit_field_names",
+      default=False,
+      action="store_true",
+      help="Whether to print field names along side values in cnn config.")
+  FLAGS, unparsed = parser.parse_known_args()
+
+  # The order of these fields must reflect the order of the structs in
+  # ${AOM_ROOT}/av1/common/cnn.h.
+  CNN_CONFIG_ORDER = collections.OrderedDict((
+      ("num_layers", 0),
+      ("is_residue", FLAGS.is_residue),
+      ("ext_width", FLAGS.ext_width),
+      ("ext_height", FLAGS.ext_height),
+      ("strict_bounds", FLAGS.strict_bounds)))
+  LAYER_CONFIG_ORDER = collections.OrderedDict((
+      ("branch", 0),
+      ("deconvolve", 0),
+      ("in_channels", -1),
+      ("filter_width", -1),
+      ("filter_height", -1),
+      ("out_channels", -1),
+      ("skip_width", 1),
+      ("skip_height", 1),
+      ("maxpool", 0),
+      ("weights", ""),
+      ("bias", ""),
+      ("pad", "PADDING_SAME_ZERO"),
+      ("activation", "NONE"),
+      ("branch_copy_mode", "COPY_NONE"),
+      ("input_to_branches", "0x00"),
+      ("channels_to_copy", 0),
+      ("branch_combine_type", "BRANCH_NOC"),
+      ("branches_to_combine", "0x00")))
+
+  app.run(main=main, argv=[sys.argv[0]] + unparsed)
