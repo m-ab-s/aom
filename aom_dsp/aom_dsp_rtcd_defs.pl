952c7ba7f22413c1c8e080722b6fb0e85b345cae
chiyotsai <chiyotsai@google.com>
2019-07-16 10:39:32 -0700

    Add sse2 assembly for highbd_sad_4xn(_avg)
    Size | Speed
    4x16 | 5.13X
    4x8  | 4.71X
    4x4  | 2.34X
    
    bug=aomedia:2432
    
    Change-Id: I87a2d4c64bfc9ec30d94b2994b3ae951af5d23e5

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -756,6 +756,8 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   specialize qw/aom_highbd_sad16x16   avx2 sse2/;
   specialize qw/aom_highbd_sad16x8    avx2 sse2/;
   specialize qw/aom_highbd_sad8x4     sse2/;
+  specialize qw/aom_highbd_sad4x8     sse2/;
+  specialize qw/aom_highbd_sad4x4     sse2/;
 
   specialize qw/aom_highbd_sad128x128_avg avx2/;
   specialize qw/aom_highbd_sad128x64_avg  avx2/;
@@ -769,13 +771,17 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   specialize qw/aom_highbd_sad16x16_avg   avx2 sse2/;
   specialize qw/aom_highbd_sad16x8_avg    avx2 sse2/;
   specialize qw/aom_highbd_sad8x4_avg     sse2/;
+  specialize qw/aom_highbd_sad4x8_avg     sse2/;
+  specialize qw/aom_highbd_sad4x4_avg     sse2/;
 
+  specialize qw/aom_highbd_sad4x16       sse2/;
   specialize qw/aom_highbd_sad16x4       sse2/;
   specialize qw/aom_highbd_sad8x32       sse2/;
   specialize qw/aom_highbd_sad32x8       sse2/;
   specialize qw/aom_highbd_sad16x64      sse2/;
   specialize qw/aom_highbd_sad64x16      sse2/;
 
+  specialize qw/aom_highbd_sad4x16_avg   sse2/;
   specialize qw/aom_highbd_sad16x4_avg   sse2/;
   specialize qw/aom_highbd_sad8x32_avg   sse2/;
   specialize qw/aom_highbd_sad32x8_avg   sse2/;
