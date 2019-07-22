cd383bae2824638d56e3f3690ba9840dfb54b91e
Sarah Parker <sarahparker@google.com>
2019-07-22 13:45:46 -0700

    Add experiment flag for new flexible transform partitions
    Change-Id: Ie0971ebfa577132ae50359d2a1689a436c09b06b

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -142,6 +142,8 @@ set_aom_config_var(CONFIG_FLEX_PARTITION 0 NUMBER
                    "AV2 Flexible partition experiment flag")
 set_aom_config_var(CONFIG_INTRA_ENTROPY 0 NUMBER
                    "AV2 intra mode entropy coding experiment flag")
+set_aom_config_var(CONFIG_NEW_TX_PARTITION 0 NUMBER
+                   "AV2 new transform partitions experiment flag")
 
 # To include Tensorflow, make sure to build tensorflow locally using
 # tensorflow/contrib/makefile/build_all_linux.sh and then providing the correct
