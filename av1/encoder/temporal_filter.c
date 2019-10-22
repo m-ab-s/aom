e842bc8c47f5eaaf5159556fc65b7fbcaa57f9a2
Yunqing Wang <yunqingwang@google.com>
2019-10-22 15:44:46 -0700

    Improve temporal filter motion search
    In temporal filtering, for a 32x32 block, use previous frame's motion
    search result mv_prev as next frame's search starting mv as long as
    mv_prev gives a small error. Also, reduce the search steps based on
    the image resolution. This improves motion search accuracy.
    
    speed 1 borg test result:
           avg_psnr: ovr_psnr: ssim:
    lowres: -0.011   -0.011   -0.016
    midres: -0.052   -0.022   -0.073
    
    Slight but not significant encoder speedup is observed.
    
    STATS_CHANGED
    
    (cherry picked from 57dbbdad1423dee9e819fa82bcc8ba83c2c6f810)
    
    Change-Id: If179bc31bf97e7ae636d4957fa010a2c22df615a

--- av1/encoder/temporal_filter.c
+++ av1/encoder/temporal_filter.c
@@ -862,21 +862,20 @@ void apply_temporal_filter_block(YV12_BUFFER_CONFIG *frame, MACROBLOCKD *mbd,
 }
 #endif  // EXPERIMENT_TEMPORAL_FILTER
 
-static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
-                                              uint8_t *arf_frame_buf,
-                                              uint8_t *frame_ptr_buf,
-                                              int stride, int x_pos, int y_pos,
-                                              MV *blk_mvs, int *blk_bestsme) {
+static int temporal_filter_find_matching_mb_c(
+    AV1_COMP *cpi, uint8_t *arf_frame_buf, uint8_t *frame_ptr_buf, int stride,
+    int x_pos, int y_pos, MV *blk_mvs, int *blk_bestsme, MV *best_ref_mv1,
+    int step_param) {
   MACROBLOCK *const x = &cpi->td.mb;
   MACROBLOCKD *const xd = &x->e_mbd;
   const MV_SPEED_FEATURES *const mv_sf = &cpi->sf.mv;
-  int step_param;
   int sadpb = x->sadperbit16;
   int bestsme = INT_MAX;
   int distortion;
   unsigned int sse;
   int cost_list[5];
   MvLimits tmp_mv_limits = x->mv_limits;
+
   xd->mi[0]->max_mv_precision = MV_SUBPEL_EIGHTH_PRECISION;
   xd->mi[0]->mv_precision = MV_SUBPEL_EIGHTH_PRECISION;
 
@@ -886,16 +885,14 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
           ? MV_COST_L1_HDRES
           : (min_frame_size >= 480 ? MV_COST_L1_MIDRES : MV_COST_L1_LOWRES);
 
-  MV best_ref_mv1 = kZeroMv;
   MV best_ref_mv1_full; /* full-pixel value of best_ref_mv1 */
-
+  MV ref_mv = kZeroMv;
   // Save input state
   struct buf_2d src = x->plane[0].src;
   struct buf_2d pre = xd->plane[0].pre[0];
   const MV_COST_TYPE ori_mv_cost_type = x->mv_cost_type;
-
-  best_ref_mv1_full.col = best_ref_mv1.col >> 3;
-  best_ref_mv1_full.row = best_ref_mv1.row >> 3;
+  best_ref_mv1_full.col = best_ref_mv1->col >> 3;
+  best_ref_mv1_full.row = best_ref_mv1->row >> 3;
 
   // Setup frame pointers
   x->plane[0].src.buf = arf_frame_buf;
@@ -903,18 +900,15 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
   xd->plane[0].pre[0].buf = frame_ptr_buf;
   xd->plane[0].pre[0].stride = stride;
 
-  step_param = mv_sf->reduce_first_step_size;
-  step_param = AOMMIN(step_param, MAX_MVSEARCH_STEPS - 2);
-
-  av1_set_mv_search_range(&x->mv_limits, &best_ref_mv1);
+  av1_set_mv_search_range(&x->mv_limits, &ref_mv);
   x->mv_cost_type = mv_cost_type;
 
   // av1_full_pixel_search() parameters: best_ref_mv1_full is the start mv, and
-  // best_ref_mv1 is for mv rate calculation. The search result is stored in
+  // ref_mv is for mv rate calculation. The search result is stored in
   // x->best_mv.
   av1_full_pixel_search(cpi, x, TF_BLOCK, &best_ref_mv1_full, step_param, 1,
                         NSTEP, 1, sadpb, cond_cost_list(cpi, cost_list),
-                        &best_ref_mv1, 0, 0, x_pos, y_pos, 0,
+                        &ref_mv, 0, 0, x_pos, y_pos, 0,
                         &cpi->ss_cfg[SS_CFG_LOOKAHEAD]);
   x->mv_cost_type = MV_COST_NONE;
   x->mv_limits = tmp_mv_limits;
@@ -942,12 +936,12 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
     return bestsme;
   }
 
-  // find_fractional_mv_step parameters: best_ref_mv1 is for mv rate cost
+  // find_fractional_mv_step parameters: ref_mv is for mv rate cost
   // calculation. The start full mv and the search result are stored in
   // x->best_mv. mi_row and mi_col are only needed for "av1_is_scaled(sf)=1"
   // case.
   bestsme = cpi->find_fractional_mv_step(
-      x, &cpi->common, 0, 0, &best_ref_mv1, xd->mi[0]->max_mv_precision,
+      x, &cpi->common, 0, 0, &ref_mv, xd->mi[0]->max_mv_precision,
       x->errorperbit, &cpi->fn_ptr[TF_BLOCK], 0, mv_sf->subpel_iters_per_step,
       cond_cost_list(cpi, cost_list), NULL, NULL,
 #if CONFIG_FLEX_MVRES
@@ -959,10 +953,10 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
 
   // DO motion search on 4 16x16 sub_blocks.
   int i, j, k = 0;
-  best_ref_mv1.row = x->e_mbd.mi[0]->mv[0].as_mv.row;
-  best_ref_mv1.col = x->e_mbd.mi[0]->mv[0].as_mv.col;
-  best_ref_mv1_full.col = best_ref_mv1.col >> 3;
-  best_ref_mv1_full.row = best_ref_mv1.row >> 3;
+  best_ref_mv1->row = x->e_mbd.mi[0]->mv[0].as_mv.row;
+  best_ref_mv1->col = x->e_mbd.mi[0]->mv[0].as_mv.col;
+  best_ref_mv1_full.col = best_ref_mv1->col >> 3;
+  best_ref_mv1_full.row = best_ref_mv1->row >> 3;
 
   for (i = 0; i < BH; i += SUB_BH) {
     for (j = 0; j < BW; j += SUB_BW) {
@@ -972,17 +966,17 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
       xd->plane[0].pre[0].buf = frame_ptr_buf + i * stride + j;
       xd->plane[0].pre[0].stride = stride;
 
-      av1_set_mv_search_range(&x->mv_limits, &best_ref_mv1);
+      av1_set_mv_search_range(&x->mv_limits, &ref_mv);
       x->mv_cost_type = mv_cost_type;
       av1_full_pixel_search(cpi, x, TF_SUB_BLOCK, &best_ref_mv1_full,
                             step_param, 1, NSTEP, 1, sadpb,
-                            cond_cost_list(cpi, cost_list), &best_ref_mv1, 0, 0,
+                            cond_cost_list(cpi, cost_list), &ref_mv, 0, 0,
                             x_pos, y_pos, 0, &cpi->ss_cfg[SS_CFG_LOOKAHEAD]);
       x->mv_limits = tmp_mv_limits;
       x->mv_cost_type = MV_COST_NONE;
 
       blk_bestsme[k] = cpi->find_fractional_mv_step(
-          x, &cpi->common, 0, 0, &best_ref_mv1, MV_SUBPEL_EIGHTH_PRECISION,
+          x, &cpi->common, 0, 0, &ref_mv, MV_SUBPEL_EIGHTH_PRECISION,
           x->errorperbit, &cpi->fn_ptr[TF_SUB_BLOCK], 0,
           mv_sf->subpel_iters_per_step, cond_cost_list(cpi, cost_list), NULL,
           NULL,
@@ -1020,6 +1014,7 @@ static FRAME_DIFF temporal_filter_iterate_c(
   const int num_planes = av1_num_planes(cm);
   const int mb_cols = get_cols(frames[alt_ref_index]->y_crop_width);
   const int mb_rows = get_rows(frames[alt_ref_index]->y_crop_height);
+  const int bd_shift = cm->seq_params.bit_depth - 8;
   int byte;
   int frame;
   int mb_col, mb_row;
@@ -1056,6 +1051,11 @@ static FRAME_DIFF temporal_filter_iterate_c(
     predictor = predictor8;
   }
 
+  const unsigned int dim = AOMMIN(frames[alt_ref_index]->y_crop_width,
+                                  frames[alt_ref_index]->y_crop_height);
+  // Decide search param based on image resolution.
+  const int step_param = av1_init_search_range(dim);
+
   mbd->block_ref_scale_factors[0] = ref_scale_factors;
   mbd->block_ref_scale_factors[1] = ref_scale_factors;
 
@@ -1083,6 +1083,7 @@ static FRAME_DIFF temporal_filter_iterate_c(
     for (mb_col = 0; mb_col < mb_cols; mb_col++) {
       int j, k;
       int stride;
+      MV best_ref_mv1 = kZeroMv;
 
       memset(accumulator, 0, BLK_PELS * 3 * sizeof(accumulator[0]));
       memset(count, 0, BLK_PELS * 3 * sizeof(count[0]));
@@ -1113,6 +1114,9 @@ static FRAME_DIFF temporal_filter_iterate_c(
           const int weight = second_alt_ref ? 4 : 2;
           blk_fw[0] = blk_fw[1] = blk_fw[2] = blk_fw[3] = weight;
           use_32x32 = 1;
+          // Change ref_mv sign for following frames.
+          best_ref_mv1.row *= -1;
+          best_ref_mv1.col *= -1;
         } else {
           int thresh_low = 10000 >> second_alt_ref;
           int thresh_high = 20000 >> second_alt_ref;
@@ -1123,7 +1127,7 @@ static FRAME_DIFF temporal_filter_iterate_c(
               cpi, frames[alt_ref_index]->y_buffer + mb_y_src_offset,
               frames[frame]->y_buffer + mb_y_src_offset,
               frames[frame]->y_stride, mb_col * BW, mb_row * BH, blk_mvs,
-              blk_bestsme);
+              blk_bestsme, &best_ref_mv1, step_param);
 
           int err16 =
               blk_bestsme[0] + blk_bestsme[1] + blk_bestsme[2] + blk_bestsme[3];
@@ -1150,6 +1154,9 @@ static FRAME_DIFF temporal_filter_iterate_c(
                               ? 2
                               : blk_bestsme[k] < thresh_high ? 1 : 0;
           }
+
+          // Don't use previous frame's mv result if error is large.
+          if (err > (3000 << bd_shift)) best_ref_mv1 = kZeroMv;
         }
 
         if (blk_fw[0] || blk_fw[1] || blk_fw[2] || blk_fw[3]) {
