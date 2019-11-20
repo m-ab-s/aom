dd327e241b1abfd7ddcee9b6619712a7d2fb4c0e
Yue Chen <yuec@google.com>
2019-11-19 16:17:36 -0800

    Add EXT_RECUR_PARTITIONS flag
    Add a config flag EXT_RECUR_PARTITIONS for AV2 fully recursive
    partitions tool.
    
    Change-Id: I457842d1d3143998c2beddd1dcde799076e30b26

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -143,6 +143,8 @@ set_aom_config_var(CONFIG_FLEX_PARTITION 0 NUMBER
 set_aom_config_var(
   CONFIG_EXT_PARTITIONS 0 NUMBER
   "AV2 3way partitions and recursive a/b partition experiment flag")
+set_aom_config_var(CONFIG_EXT_RECUR_PARTITIONS 0 NUMBER
+                   "AV2 Fully recursive partitions experiment flag")
 set_aom_config_var(CONFIG_INTRA_ENTROPY 0 NUMBER
                    "AV2 intra mode entropy coding experiment flag")
 set_aom_config_var(CONFIG_USE_SMALL_MODEL 1 NUMBER
