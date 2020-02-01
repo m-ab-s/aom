e1b3883a291de884ea8598dead71e357af5c7501
chiyotsai <chiyotsai@google.com>
2020-01-31 16:31:21 -0800

    Use kZeroMv as refmv for mv rate cost in temporal filter
    STATS_CHANGED
    
    Change-Id: I5c9af85bd2ae0ab25369cf55c9f380c975975419

--- av1/encoder/temporal_filter.c
+++ av1/encoder/temporal_filter.c
@@ -899,8 +899,8 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
   // best_ref_mv1 is for mv rate calculation. The search result is stored in
   // x->best_mv.
   av1_full_pixel_search(cpi, x, TF_BLOCK, &best_ref_mv1_full, step_param, NSTEP,
-                        1, sadpb, cond_cost_list(cpi, cost_list), &best_ref_mv1,
-                        0, 0, x_pos, y_pos, 0, &cpi->ss_cfg[SS_CFG_LOOKAHEAD]);
+                        1, sadpb, cond_cost_list(cpi, cost_list), &kZeroMv, 0,
+                        0, x_pos, y_pos, 0, &cpi->ss_cfg[SS_CFG_LOOKAHEAD]);
   x->mv_limits = tmp_mv_limits;
 
   // Ignore mv costing by sending NULL pointer instead of cost array
@@ -959,7 +959,7 @@ static int temporal_filter_find_matching_mb_c(AV1_COMP *cpi,
       av1_set_mv_search_range(&x->mv_limits, &best_ref_mv1);
       av1_full_pixel_search(cpi, x, TF_SUB_BLOCK, &best_ref_mv1_full,
                             step_param, NSTEP, 1, sadpb,
-                            cond_cost_list(cpi, cost_list), &best_ref_mv1, 0, 0,
+                            cond_cost_list(cpi, cost_list), &kZeroMv, 0, 0,
                             x_pos, y_pos, 0, &cpi->ss_cfg[SS_CFG_LOOKAHEAD]);
       x->mv_limits = tmp_mv_limits;
 
