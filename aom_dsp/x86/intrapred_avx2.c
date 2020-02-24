24ce990ef612bd2bb8236d94a47bfa085761cef5
Elliott Karpilovsky <elliottk@google.com>
2020-02-24 12:43:07 -0800

    Change intrapred stores to unaligned
    Trying to fix the issue that yuec@ encountered with aligned
    stores causing segfaults.
    
    Change-Id: Idb8bd6759b3d0d43210ca9344a7aca7fb79772a9

--- aom_dsp/x86/intrapred_avx2.c
+++ aom_dsp/x86/intrapred_avx2.c
@@ -794,7 +794,7 @@ void aom_paeth_predictor_16x8_avx2(uint8_t *dst, ptrdiff_t stride,
     const __m256i l16 = _mm256_shuffle_epi8(l, rep);
     const __m128i row = paeth_16x1_pred(&l16, &top, &tl16);
 
-    _mm_store_si128((__m128i *)dst, row);
+    _mm_storeu_si128((__m128i *)dst, row);
     dst += stride;
     rep = _mm256_add_epi16(rep, one);
   }
