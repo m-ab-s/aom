8f7c0acf538fbbcb90873c8b3f3d45630237f21b
Mufaddal Chakera <mufaddal.chakera@ittiam.com>
2020-03-10 15:46:17 +0530

    Correct frames_to_key in single_pass for last GOP
    Correct the value of frames_to_key when flush condition
    is reached(towards the last GOP). Because flush has begun
    we can be sure that no more input frames remain, and we can
    use the depth of the lookahead buffer to determine frames_to_key.
    
    Change-Id: I8542bdd69b6297df2fe19b227059570fa2e35e4c

--- av1/encoder/pass2_strategy.c
+++ av1/encoder/pass2_strategy.c
@@ -784,20 +784,23 @@ static INLINE int is_almost_static(double gf_zero_motion, int kf_zero_motion) {
 #endif  // GROUP_ADAPTIVE_MAXQ
 #define MIN_FWD_KF_INTERVAL 8
 
+static void correct_frames_to_key(AV1_COMP *cpi) {
+  int lookahead_size =
+      (int)av1_lookahead_depth(cpi->lookahead, cpi->compressor_stage) + 1;
+  if (lookahead_size <
+      av1_lookahead_pop_sz(cpi->lookahead, cpi->compressor_stage)) {
+    cpi->rc.frames_to_key = AOMMIN(cpi->rc.frames_to_key, lookahead_size);
+  }
+}
+
 static void define_gf_group_pass0(AV1_COMP *cpi,
                                   const EncodeFrameParams *const frame_params) {
   RATE_CONTROL *const rc = &cpi->rc;
   GF_GROUP *const gf_group = &cpi->gf_group;
   int target;
 
-  // If this is last GF group of the video, correct frames_to_key if needed.
-  const int lookahead_size =
-      (int)av1_lookahead_depth(cpi->lookahead, ENCODE_STAGE);
-  if (cpi->oxcf.lag_in_frames > 0 && cpi->lookahead != NULL &&
-      lookahead_size + 1 < cpi->oxcf.lag_in_frames) {
-    // This must be the last GF group in the video.
-    rc->frames_to_key = AOMMIN(rc->frames_to_key, lookahead_size + 1);
-  }
+  // correct frames_to_key when lookahead queue is flushing
+  correct_frames_to_key(cpi);
 
   if (cpi->oxcf.aq_mode == CYCLIC_REFRESH_AQ)
     av1_cyclic_refresh_set_golden_update(cpi);
@@ -949,6 +952,11 @@ static void define_gf_group(AV1_COMP *cpi, FIRSTPASS_STATS *this_frame,
   if (has_no_stats_stage(cpi)) {
     define_gf_group_pass0(cpi, frame_params);
     return;
+  }
+
+  // correct frames_to_key when lookahead queue is emptying
+  if (cpi->lap_enabled) {
+    correct_frames_to_key(cpi);
   }
 
   // Load stats for the current frame.
@@ -1503,6 +1511,7 @@ static void find_next_key_frame(AV1_COMP *cpi, FIRSTPASS_STATS *this_frame) {
       rc->frames_to_key = num_frames_to_app_forced_key;
     else
       rc->frames_to_key = AOMMAX(1, cpi->oxcf.key_freq);
+    correct_frames_to_key(cpi);
     rc->kf_boost = DEFAULT_KF_BOOST;
     rc->source_alt_ref_active = 0;
     gf_group->update_type[0] = KF_UPDATE;
@@ -1600,7 +1609,9 @@ static void find_next_key_frame(AV1_COMP *cpi, FIRSTPASS_STATS *this_frame) {
       rc->frames_to_key = num_frames_to_app_forced_key;
     else
       rc->frames_to_key = AOMMAX(1, cpi->oxcf.key_freq);
+    correct_frames_to_key(cpi);
   }
+
   // If there is a max kf interval set by the user we must obey it.
   // We already breakout of the loop above at 2x max.
   // This code centers the extra kf if the actual natural interval
