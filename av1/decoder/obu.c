e6365bff49b3239ef1187354685d3554c054686e
Wan-Teh Chang <wtc@google.com>
2020-08-27 15:49:56 -0700

    Implement the frame_header_copy check in the spec.
    This is the av1-normative branch version of
    https://aomedia-review.googlesource.com/c/aom/+/118165.
    
    Whenever we receive an OBU_REDUNDANT_FRAME_HEADER, verify that its
    contents are identical to the contents of the OBU_FRAME_HEADER of the
    current frame.
    
    BUG=aomedia:2814
    
    Change-Id: I13e806997ed9110776529e84e0116c4f88967d5c

--- av1/decoder/obu.c
+++ av1/decoder/obu.c
@@ -802,7 +802,17 @@ int aom_decode_frame_from_obus(struct AV1Decoder *pbi, const uint8_t *data,
   AV1_COMMON *const cm = &pbi->common;
   int frame_decoding_finished = 0;
   int is_first_tg_obu_received = 1;
-  int frame_header_size = 0;
+  // Whenever pbi->seen_frame_header is set to 1, frame_header is set to the
+  // beginning of the frame_header_obu and frame_header_size is set to its
+  // size. This allows us to check if a redundant frame_header_obu is a copy
+  // of the previous frame_header_obu.
+  //
+  // Initialize frame_header to a dummy nonnull pointer, otherwise the Clang
+  // Static Analyzer in clang 7.0.1 will falsely warn that a null pointer is
+  // passed as an argument to a 'nonnull' parameter of memcmp(). The initial
+  // value will not be used.
+  const uint8_t *frame_header = data;
+  uint32_t frame_header_size = 0;
   ObuHeader obu_header;
   memset(&obu_header, 0, sizeof(obu_header));
   pbi->seen_frame_header = 0;
@@ -896,10 +906,22 @@ int aom_decode_frame_from_obus(struct AV1Decoder *pbi, const uint8_t *data,
           pbi->seen_frame_header = 1;
           frame_header_size = read_frame_header_obu(
               pbi, &rb, data, p_data_end, obu_header.type != OBU_FRAME);
+          frame_header = data;
           if (cm->large_scale_tile) pbi->camera_frame_header_ready = 1;
+        } else {
+          // Verify that the frame_header_obu is identical to the original
+          // frame_header_obu.
+          if (frame_header_size > payload_size ||
+              memcmp(data, frame_header, frame_header_size) != 0) {
+            cm->error.error_code = AOM_CODEC_CORRUPT_FRAME;
+            return -1;
+          }
+          assert(rb.bit_offset == 0);
+          rb.bit_offset = 8 * frame_header_size;
         }
+
         decoded_payload_size = frame_header_size;
-        pbi->frame_header_size = (size_t)frame_header_size;
+        pbi->frame_header_size = frame_header_size;
 
         if (cm->show_existing_frame) {
           if (obu_header.type == OBU_FRAME) {
