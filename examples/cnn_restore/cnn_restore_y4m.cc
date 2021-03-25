c8d91fb169ed1c49fc0cd50e949f756c9fba4f63
Debargha Mukherjee <debargha@google.com>
2021-03-25 12:08:20 -0700

    Extend cnn_restore utility to cover more ratios
    Also, the models are smaller than before with ~10K params each.
    The interface is changed to provide the upsampling_ratio
    rather than index to hidden models.
    
    Adds a down-up-superres script for convenience of testing.
    
    Change-Id: I5ec735a464ab9ae9b35922506be05f4f45724f95
    (cherry picked from commit 4e7e435481adc25ec14ea53314e7a49c5a71cbb5)

--- examples/cnn_restore/cnn_restore_y4m.cc
+++ examples/cnn_restore/cnn_restore_y4m.cc
@@ -18,6 +18,9 @@
 
 #include "common/tf_lite_includes.h"
 
+#define CFG_MAX_LEN 256
+#define NUM_MODELS 8
+
 #define Y4M_HDR_MAX_LEN 256
 #define Y4M_HDR_MAX_WORDS 16
 #define NUM_THREADS 8
@@ -29,7 +32,7 @@
 //   cnn_restore_y4m
 //       <y4m_input>
 //       <num_frames>
-//       <restore_code>
+//       <upsampling_ratio>
 //       <y4m_output>
 
 namespace {
@@ -38,6 +41,10 @@ namespace {
 #include "examples/cnn_restore/sr3by2_tflite.h"
 #include "examples/cnn_restore/sr4by3_tflite.h"
 #include "examples/cnn_restore/sr5by4_tflite.h"
+#include "examples/cnn_restore/sr6by5_tflite.h"
+#include "examples/cnn_restore/sr7by6_tflite.h"
+#include "examples/cnn_restore/sr8by7_tflite.h"
+#include "examples/cnn_restore/sr9by8_tflite.h"
 
 void RegisterSelectedOps(::tflite::MutableOpResolver *resolver) {
   resolver->AddBuiltin(::tflite::BuiltinOperator_ADD,
@@ -57,13 +64,9 @@ static void usage_and_exit(char *prog) {
   printf("  %s\n", prog);
   printf("      <y4m_input>\n");
   printf("      <num_frames>\n");
-  printf("      <restore_code>\n");
-  printf("          Possible values:\n");
-  printf("              0 - none [default]\n");
-  printf("              1 - 2/1 x 2/1 superresolution\n");
-  printf("              2 - 3/2 x 3/2 superresolution\n");
-  printf("              3 - 4/3 x 4/3 superresolution\n");
-  printf("              4 - 5/4 x 5/4 superresolution\n");
+  printf("      <upsampling_ratio>\n");
+  printf("          in form <p>:<q> where <p>/<q> is the upsampling ratio\n");
+  printf("          with <p> greater than <q>.\n");
   printf("      <y4m_output>\n");
   printf("      \n");
   exit(EXIT_FAILURE);
@@ -82,6 +85,20 @@ static int split_words(char *buf, char delim, int nmax, char **words) {
   words[n++] = y;
   assert(n > 0 && n <= nmax);
   return n;
+}
+
+static int parse_rational_config(char *cfg, int *p, int *q) {
+  char cfgbuf[CFG_MAX_LEN];
+  strncpy(cfgbuf, cfg, CFG_MAX_LEN - 1);
+
+  char *cfgwords[2];
+  const int ncfgwords = split_words(cfgbuf, ':', 2, cfgwords);
+  if (ncfgwords < 2) return 0;
+
+  *p = atoi(cfgwords[0]);
+  *q = atoi(cfgwords[1]);
+  if (*p <= 0 || *q <= 0 || *p < *q) return 0;
+  return 1;
 }
 
 static int parse_info(char *hdrwords[], int nhdrwords, int *width, int *height,
@@ -112,15 +129,40 @@ static int parse_info(char *hdrwords[], int nhdrwords, int *width, int *height,
   return 1;
 }
 
+static const double model_ratios[NUM_MODELS] = { 2.0 / 1.0, 3.0 / 2.0,
+                                                 4.0 / 3.0, 5.0 / 4.0,
+                                                 6.0 / 5.0, 7.0 / 6.0,
+                                                 8.0 / 7.0, 9.0 / 8.0 };
+
 static const unsigned char *get_model(int code) {
   switch (code) {
-    case 0: return NULL;
-    case 1: return _tmp_sr2by1_tflite;
-    case 2: return _tmp_sr3by2_tflite;
-    case 3: return _tmp_sr4by3_tflite;
-    case 4: return _tmp_sr5by4_tflite;
+    case -1: return NULL;
+    case 0: return _tmp_sr2by1_tflite;
+    case 1: return _tmp_sr3by2_tflite;
+    case 2: return _tmp_sr4by3_tflite;
+    case 3: return _tmp_sr5by4_tflite;
+    case 4: return _tmp_sr6by5_tflite;
+    case 5: return _tmp_sr7by6_tflite;
+    case 6: return _tmp_sr8by7_tflite;
+    case 7: return _tmp_sr9by8_tflite;
     default: return NULL;
   }
+}
+
+static int search_best_model(int p, int q) {
+  if (p == q) return -1;
+  double ratio = (double)p / q;
+  // assume -1 corresponds to ratio of 1
+  int mini = -1;
+  double minerr = fabs(ratio - 1.0);
+  for (int i = 0; i < NUM_MODELS; ++i) {
+    double err = fabs(ratio - model_ratios[i]);
+    if (err < minerr) {
+      mini = i;
+      minerr = err;
+    }
+  }
+  return mini;
 }
 
 static TfLiteDelegate *get_tflite_xnnpack_delegate(int num_threads) {
@@ -181,6 +223,7 @@ static inline uint16_t clip_pixel_highbd(int x, int bd) {
 static int restore_cnn_img_tflite_lowbd(
     const std::unique_ptr<tflite::Interpreter> &interpreter, const uint8_t *dgd,
     int width, int height, int dgd_stride, uint8_t *rst, int rst_stride) {
+  if (interpreter == nullptr) return 0;
   // Prepare input.
   const float max_val = 255.0f;
   const int in_stride = width;
@@ -217,6 +260,7 @@ static int restore_cnn_img_tflite_highbd(
     const std::unique_ptr<tflite::Interpreter> &interpreter,
     const uint16_t *dgd, int width, int height, int dgd_stride, uint16_t *rst,
     int rst_stride, int bit_depth) {
+  if (interpreter == nullptr) return 0;
   // Prepare input.
   const auto max_val = static_cast<float>((1 << bit_depth) - 1);
   const int in_stride = width;
@@ -286,7 +330,14 @@ int main(int argc, char *argv[]) {
   }
   const int bytes_per_pel = (bitdepth + 7) / 8;
   int num_frames = atoi(argv[2]);
-  const int restore_code = atoi(argv[3]);
+  int p, q;
+  if (!parse_rational_config(argv[3], &p, &q)) {
+    printf("Could not parse upsampling factor from %s\n", argv[3]);
+    usage_and_exit(argv[0]);
+  }
+  const int restore_code = search_best_model(p, q);
+  printf("best_model = %d (ratio %f)\n", restore_code,
+         restore_code == -1 ? 1.0 : model_ratios[restore_code]);
 
   const int uvwidth = subx ? (ywidth + 1) >> 1 : ywidth;
   const int uvheight = suby ? (yheight + 1) >> 1 : yheight;
@@ -317,14 +368,22 @@ int main(int argc, char *argv[]) {
     }
     if (fread(inbuf, (ysize + 2 * uvsize) * bytes_per_pel, 1, fin) != 1) break;
     if (bytes_per_pel == 1) {
-      restore_cnn_img_tflite_lowbd(interpreter, inbuf, ywidth, yheight, ywidth,
-                                   outbuf, ywidth);
+      if (interpreter != nullptr) {
+        restore_cnn_img_tflite_lowbd(interpreter, inbuf, ywidth, yheight,
+                                     ywidth, outbuf, ywidth);
+      } else {
+        memcpy(outbuf, inbuf, ysize * bytes_per_pel);
+      }
       memcpy(outbuf + ysize * bytes_per_pel, inbuf + ysize * bytes_per_pel,
              2 * uvsize * bytes_per_pel);
     } else {
-      restore_cnn_img_tflite_highbd(interpreter, (uint16_t *)inbuf, ywidth,
-                                    yheight, ywidth, (uint16_t *)outbuf, ywidth,
-                                    bitdepth);
+      if (interpreter != nullptr) {
+        restore_cnn_img_tflite_highbd(interpreter, (uint16_t *)inbuf, ywidth,
+                                      yheight, ywidth, (uint16_t *)outbuf,
+                                      ywidth, bitdepth);
+      } else {
+        memcpy(outbuf, inbuf, ysize * bytes_per_pel);
+      }
       memcpy(outbuf + ysize * bytes_per_pel, inbuf + ysize * bytes_per_pel,
              2 * uvsize * bytes_per_pel);
     }
