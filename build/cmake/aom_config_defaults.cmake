30874c1f2bc501266e0c8d237f675e8c0d41fc87
oguleryuz <oguleryuz@google.com>
2021-05-05 16:50:20 -0700

    Adds nonseparable Wiener filter config variables.
    Change-Id: I73d3dd073598b7005f608f343d3a9eaff915c040

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -143,6 +143,10 @@ set_aom_config_var(CONFIG_LOOP_RESTORE_CNN 0 NUMBER
                    "AV2 CNN in Loop Restoration experiment flag")
 set_aom_config_var(CONFIG_CNN_RESTORATION_SMALL_MODELS 0 NUMBER
                    "AV2 CNN restoration with smaller models experiment flag")
+set_aom_config_var(CONFIG_WIENER_NONSEP 0 NUMBER
+                   "AV2 nonsep Wiener filter experiment flag")
+set_aom_config_var(CONFIG_WIENER_NONSEP_CROSS_FILT 0 NUMBER
+                   "AV2 nonsep Wiener cross filter experiment flag")
 
 #
 # Variables in this section control optional features of the build system.
