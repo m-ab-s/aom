c9939b31c2c67ecd613f716119851726a9ae6997
Urvang Joshi <urvang@google.com>
2021-06-08 12:39:33 -0700

    CNN Restoration: Remove bigger models.
    Keep only the models used by CNN_RESTORATION_SMALL_MODELS, and remove
    that flag.
    
    Change-Id: I8beb2f491d58ed4a29f27dfb9923b4e62883dbd6

--- build/cmake/aom_experiment_deps.cmake
+++ build/cmake/aom_experiment_deps.cmake
@@ -30,13 +30,6 @@ macro(fix_experiment_configs)
                            CONFIG_WIENER_NONSEP_CROSS_FILT)
   endif()
 
-  if(CONFIG_CNN_RESTORATION_SMALL_MODELS)
-    if(NOT (CONFIG_CNN_RESTORATION))
-      change_config_and_warn(CONFIG_CNN_RESTORATION 1
-                             CONFIG_CNN_RESTORATION_SMALL_MODELS)
-    endif()
-  endif()
-
   if(CONFIG_CNN_RESTORATION)
     change_config_and_warn(CONFIG_TENSORFLOW_LITE 1 CONFIG_CNN_RESTORATION)
   endif()
