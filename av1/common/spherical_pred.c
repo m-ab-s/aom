79687206febe3225ecf077a86daf956917aa7b7d
yaoyaogoogle@google.com <yaoyaogoogle@google.com>
2021-06-11 19:37:56 +0000

    Add spherical coordinate range control
    Change-Id: Ibd9ef4b7e0c47ea4b43d727c5d551de3a8d5e4b1

--- av1/common/spherical_pred.c
+++ av1/common/spherical_pred.c
@@ -14,26 +14,47 @@
 
 #include "av1/common/common.h"
 #include "av1/common/spherical_pred.h"
+#include "aom_dsp/aom_dsp_common.h"
 
 void av1_sphere_to_plane_erp(double phi, double theta, int width, int height,
                              double *x, double *y) {
-  // The 0.01 is for issues when comparing floating point numbers
-  // TODO(yaoyaogoogle): Find a better way to solve the issue
-  assert(phi >= -PI * 0.5 - 0.01 && phi <= PI * 0.5 + 0.01 &&
-         theta >= -PI - 0.01 && theta <= PI + 0.01);
+  double phi_mod = fmod(phi, 2 * PI);
+  int is_on_other_hemisphere = ((phi_mod > 0.5 * PI && phi_mod < 1.5 * PI) ||
+                                (phi_mod < -0.5 * PI && phi_mod > -1.5 * PI));
 
+  // Flip phi to the destination quadrant
+  phi_mod = is_on_other_hemisphere ? PI - phi_mod : phi_mod;
+  // Regualte phi back to [-pi/2, pi/2)
+  if (phi_mod > 1.5 * PI) {
+    phi_mod = phi_mod - 2 * PI;
+  } else if (phi_mod < -1.5 * PI) {
+    phi_mod = phi_mod + 2 * PI;
+  }
+
+  double theta_mod = fmod(is_on_other_hemisphere ? theta + PI : theta, 2 * PI);
+  if (theta_mod < -PI) {
+    theta_mod = theta_mod + 2 * PI;
+  } else if (theta_mod >= PI) {
+    theta_mod = theta_mod - 2 * PI;
+  }
   // This should actually be a range related to 1/cos(phi) since x is distorted
   // TODO(yaoyaogoogle): Adjust the width of x according to the interpolation
   // mode
-  *x = theta / PI * width * 0.5 + width * 0.5;
+  *x = theta_mod / PI * width * 0.5 + width * 0.5;
+
   // No minus sign for y since we only use an imaginary upside-down globe
-  *y = phi / (PI * 0.5) * height * 0.5 + height * 0.5;
+  *y = phi_mod / (PI * 0.5) * height * 0.5 + height * 0.5;
+  *y = AOMMIN(*y, height - 0.00001);
 }
 
 void av1_plane_to_sphere_erp(double x, double y, int width, int height,
                              double *phi, double *theta) {
-  assert(x < width && x >= 0 && y < height && y >= 0);
+  assert(y >= 0 && y < height);
 
-  *theta = (x - width * 0.5) / width * 2 * PI;
+  double x_mod = fmod(x, width);
+  x_mod = x_mod >= 0 ? x_mod : x_mod + width;
+
+  // Since x_mod is in [0, width), theta is in [-PI, PI)
+  *theta = (x_mod - width * 0.5) / width * 2 * PI;
   *phi = (y - height * 0.5) / height * PI;
 }
--- av1/common/spherical_pred.h
+++ av1/common/spherical_pred.h
@@ -17,23 +17,27 @@ extern "C" {
 #endif
 
 /*!\brief Convert equirectangular coordinate to plane
- * \param[in]   phi     The latitude in radian ([-pi/2, pi/2])
- * \param[in]   theta   The longitude in radian ([-pi, pi])
+ * \param[in]   phi     The latitude in radian. When phi passes polars,
+ *                      theta will go to the other hemisphere (+ pi).
+ * \param[in]   theta   The longitude in radian. If it reaches
+ *                      (2n + 1) * pi, it will be treated as -pi.
  * \param[in]   width   The width of the frame
  * \param[in]   height  The height of the frame
  * \param[out]  x       The X coordinate on the plane
- * \param[out]  y       The Y coordinate on the plane
+ * \param[out]  y       The Y coordinate on the plane. It will be slightly
+ *                      less than height even when phi = 0.5 * pi.
  */
 void av1_sphere_to_plane_erp(double phi, double theta, int width, int height,
                              double *x, double *y);
 
 /*!\brief Convert plane coordinate to equirectangular
- * \param[in]  x       The X coordinate on the plane
- * \param[in]  y       The Y coordinate on the plane
+ * \param[in]  x       The X coordinate on the plane, will be warpped
+ *                     to the other side if < 0 or >= width.
+ * \param[in]  y       The Y coordinate on the plane ([0, height))
  * \param[in]  width   The width of the frame
  * \param[in]  height  The height of the frame
- * \param[out] phi     The latitude in radian ([-pi/2, pi/2])
- * \param[out] theta   The longitude in radian ([-pi, pi])
+ * \param[out] phi     The latitude in radian ([-pi/2, pi/2))
+ * \param[out] theta   The longitude in radian ([-pi, pi))
  */
 void av1_plane_to_sphere_erp(double x, double y, int width, int height,
                              double *phi, double *theta);
