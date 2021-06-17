98a9e6bb687fc9c32744eeef0bc8b020a5a7ae47
yaoyaogoogle@google.com <yaoyaogoogle@google.com>
2021-06-17 22:50:03 +0000

    Get referecence block via spherical motion vector
    Change-Id: Ia4b86d8aef2db6a1d91953f6fe5bae5e339c785c

--- av1/common/spherical_pred.c
+++ av1/common/spherical_pred.c
@@ -9,7 +9,6 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
-#include <assert.h>
 #include <math.h>
 
 #include "av1/common/common.h"
@@ -49,7 +48,8 @@ void av1_sphere_to_plane_erp(double phi, double theta, int width, int height,
 
 void av1_plane_to_sphere_erp(double x, double y, int width, int height,
                              double *phi, double *theta) {
-  assert(y >= 0 && y < height);
+  y = AOMMAX(y, 0);
+  y = AOMMIN(y, height - 1);
 
   double x_mod = fmod(x, width);
   x_mod = x_mod >= 0 ? x_mod : x_mod + width;
@@ -57,4 +57,44 @@ void av1_plane_to_sphere_erp(double x, double y, int width, int height,
   // Since x_mod is in [0, width), theta is in [-PI, PI)
   *theta = (x_mod - width * 0.5) / width * 2 * PI;
   *phi = (y - height * 0.5) / height * PI;
+}
+
+void av1_get_pred_erp(int block_x, int block_y, int block_width,
+                      int block_height, double delta_phi, double delta_theta,
+                      uint8_t *ref_frame, int ref_frame_stride, int frame_width,
+                      int frame_height, int pred_block_stride,
+                      uint8_t *pred_block) {
+  assert(ref_frame != NULL && frame_width > 0 && frame_height > 0 &&
+         ref_frame_stride >= frame_width);
+  assert(pred_block != NULL && block_width > 0 && block_height > 0 &&
+         pred_block_stride >= block_width);
+
+  int pos_pred;      // Offset in pred_block buffer
+  int pos_ref;       // Offset in ref_frame buffer
+  double phi;        // Latitude on the globe
+  double theta;      // Longitude on the globe
+  double x_current;  // X coordiante in current frame
+  double y_current;  // Y coordiante in currrent frame
+  double x_ref;      // X coordinate in reference frame
+  double y_ref;      // Y coordiante in reference frame
+
+  for (int idx_y = 0; idx_y < block_height; idx_y++) {
+    for (int idx_x = 0; idx_x < block_width; idx_x++) {
+      x_current = idx_x + block_x;
+      y_current = idx_y + block_y;
+
+      av1_plane_to_sphere_erp(x_current, y_current, frame_width, frame_height,
+                              &phi, &theta);
+      av1_sphere_to_plane_erp(phi + delta_phi, theta + delta_theta, frame_width,
+                              frame_height, &x_ref, &y_ref);
+
+      pos_pred = idx_x + idx_y * pred_block_stride;
+      x_ref = round(x_ref);
+      x_ref = x_ref == frame_width ? 0 : x_ref;
+      y_ref = round(y_ref);
+      y_ref = y_ref == frame_height ? frame_height - 1 : y_ref;
+      pos_ref = (int)x_ref + (int)y_ref * ref_frame_stride;
+      pred_block[pos_pred] = ref_frame[pos_ref];
+    }
+  }
 }
--- av1/common/spherical_pred.h
+++ av1/common/spherical_pred.h
@@ -16,6 +16,8 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+
 /*!\brief Convert equirectangular coordinate to plane
  * \param[in]   phi     The latitude in radian. When phi passes polars,
  *                      theta will go to the other hemisphere (+ pi).
@@ -41,6 +43,30 @@ void av1_sphere_to_plane_erp(double phi, double theta, int width, int height,
  */
 void av1_plane_to_sphere_erp(double x, double y, int width, int height,
                              double *phi, double *theta);
+
+/*!\brief Given a spherical motion vector (delat_phi, delta_theta) and a
+ * block on the current frame, find the corresponding pixels in the
+ * reference frame for each pixel in the given block.
+ * \param[in]   block_x             Block's upper left corner X on the plane
+ * \param[in]   block_y             Block's upper left corner Y on the plane
+ * \param[in]   block_width         Width of the block
+ * \param[in]   block_height        Height of the block
+ * \param[in]   delta_phi           Latitude motion vector, added to the
+ *                                  latitude of each pixel in the block
+ * \param[in]   delta_theta         Longitude motion vector, added to the
+ *                                  longitude of each pixel in the block
+ * \param[in]   ref_frame           Reference frame data
+ * \param[in]   ref_frame_stride    Stride of reference frame data
+ * \param[in]   frame_width         Width of frame
+ * \param[in]   frame_height        Height of frame
+ * \param[in]   pred_block_stride   Stride of the predicted block
+ * \param[out]  pred_block          Predicted block
+ */
+void av1_get_pred_erp(int block_x, int block_y, int block_width,
+                      int block_height, double delta_phi, double delta_theta,
+                      uint8_t *ref_frame, int ref_frame_stride, int frame_width,
+                      int frame_height, int pred_block_stride,
+                      uint8_t *pred_block);
 
 #ifdef __cplusplus
 }  // extern "C"
