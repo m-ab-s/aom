67947952a9373df0673626d62d4013c1aeb7569d
Debargha Mukherjee <debargha@google.com>
2021-06-18 08:39:59 -0700

    Turn CONFIG_NEW_INTER_MODES on by default
    Increase parameter for warning=stack-usage for compiler. Some
    increase in stack usage with CONFIG_RD_DEBUG is expected.
    
    STATS_CHANGED
    
    Change-Id: I06546724c73d9ea8e41cf3eb729113ad8d5c918d

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -154,7 +154,7 @@ set_aom_config_var(
   "AV2 experiment flag to enable offset based refinement of intra prediction.")
 set_aom_config_var(CONFIG_IST 1 NUMBER
                    "AV2 experiment flag to enable intra secondary transform.")
-set_aom_config_var(CONFIG_NEW_INTER_MODES 0 NUMBER
+set_aom_config_var(CONFIG_NEW_INTER_MODES 1 NUMBER
                    "AV2 inter mode consolidation experiment flag")
 set_aom_config_var(CONFIG_SMVP_IMPROVEMENT 0 "Enable SMVP improvement")
 set_aom_config_var(CONFIG_TMVP_IMPROVEMENT 0 "Enable TMVP improvement")
