b0d5a421b90d054ffcea1a2d02de4d3ac9e443f5
Sarah Parker <sarahparker@google.com>
2021-07-12 17:40:21 -0700

    Enable compiler flag for NEW_TX_PARTITION
    STATS_CHANGED
    
    Change-Id: I936fab3f1a6d27e7826c6ccea9cd791de178d237

--- build/cmake/aom_config_defaults.cmake
+++ build/cmake/aom_config_defaults.cmake
@@ -145,7 +145,7 @@ set_aom_config_var(CONFIG_EXTQUANT 1
                    "AV2 extended quantization experiment flag")
 set_aom_config_var(CONFIG_COMPOUND_WARP_SAMPLES 1 NUMBER
                    "AV2 compound warped motion samples experiment flag")
-set_aom_config_var(CONFIG_NEW_TX_PARTITION 0
+set_aom_config_var(CONFIG_NEW_TX_PARTITION 1
                    "AV2 new transform partitions experiment flag.")
 set_aom_config_var(
   CONFIG_MRLS 1 "AV2 experiment flag to enable multiple line intra prediction.")
