e4f947969797729b4a9eaf3429536c9203ef0b7b
Urvang Joshi <urvang@google.com>
2021-08-23 14:02:24 -0700

    CNN Restoration: int16x8 quant for Y models
    Change-Id: I20ca088be150312024a234fdab960fb37bfb85df

--- av1/tflite_models/op_registrations.cc
+++ av1/tflite_models/op_registrations.cc
@@ -4,13 +4,18 @@
 
 void RegisterSelectedOpsAllQps(::tflite::MutableOpResolver *resolver) {
   resolver->AddBuiltin(::tflite::BuiltinOperator_ADD,
-                       ::tflite::ops::builtin::Register_ADD());
+                       ::tflite::ops::builtin::Register_ADD(), 1, 2);
   resolver->AddBuiltin(::tflite::BuiltinOperator_CONV_2D,
-                       ::tflite::ops::builtin::Register_CONV_2D());
+                       ::tflite::ops::builtin::Register_CONV_2D(), 1, 3);
   resolver->AddBuiltin(::tflite::BuiltinOperator_DEPTHWISE_CONV_2D,
-                       ::tflite::ops::builtin::Register_DEPTHWISE_CONV_2D());
+                       ::tflite::ops::builtin::Register_DEPTHWISE_CONV_2D(), 1,
+                       3);
+  resolver->AddBuiltin(::tflite::BuiltinOperator_DEQUANTIZE,
+                       ::tflite::ops::builtin::Register_DEQUANTIZE(), 2, 2);
   resolver->AddBuiltin(::tflite::BuiltinOperator_MIRROR_PAD,
                        ::tflite::ops::builtin::Register_MIRROR_PAD());
   resolver->AddBuiltin(::tflite::BuiltinOperator_PAD,
-                       ::tflite::ops::builtin::Register_PAD());
+                       ::tflite::ops::builtin::Register_PAD(), 1, 2);
+  resolver->AddBuiltin(::tflite::BuiltinOperator_QUANTIZE,
+                       ::tflite::ops::builtin::Register_QUANTIZE());
 }
