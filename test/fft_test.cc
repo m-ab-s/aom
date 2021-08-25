223fba6679eac18254ab2a71c0840297b39121a5
Wan-Teh Chang <wtc@google.com>
2021-08-25 15:14:13 -0700

    Fix clang -Wunused-but-set-variable warnings
    Fix clang -Wunused-but-set-variable warnings about the local variable
    'sum' in the FFT2DTest.Benchmark and IFFT2DTest.Benchmark tests.
    
    The fix was suggested by Neil Birkbeck.
    
    Change-Id: I4e4c35e409e90edcd144016403f945be41d683e7
    (cherry picked from commit e651c855666d9eab78897142e0728f654b26e043)

--- test/fft_test.cc
+++ test/fft_test.cc
@@ -130,12 +130,14 @@ TEST_P(FFT2DTest, Benchmark) {
 
   int n = GetParam().n;
   float sum = 0;
-  for (int i = 0; i < 1000 * (64 - n); ++i) {
+  const int num_trials = 1000 * (64 - n);
+  for (int i = 0; i < num_trials; ++i) {
     input_[i % (n * n)] = 1;
     GetParam().fft(&input_[0], &temp_[0], &output_[0]);
     sum += output_[0];
     input_[i % (n * n)] = 0;
   }
+  EXPECT_NEAR(sum, num_trials, 1e-3);
 }
 
 INSTANTIATE_TEST_CASE_P(
@@ -235,12 +237,14 @@ TEST_P(IFFT2DTest, Benchmark) {
   if (disabled_) return;
   int n = GetParam().n;
   float sum = 0;
-  for (int i = 0; i < 1000 * (64 - n); ++i) {
+  const int num_trials = 1000 * (64 - n);
+  for (int i = 0; i < num_trials; ++i) {
     input_[i % (n * n)] = 1;
     GetParam().ifft(&input_[0], &temp_[0], &output_[0]);
     sum += output_[0];
     input_[i % (n * n)] = 0;
   }
+  EXPECT_GE(sum, num_trials / 2);
 }
 INSTANTIATE_TEST_CASE_P(
     IFFT2DTestC, IFFT2DTest,
