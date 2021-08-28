a319a0f616473c6abc85ecb8b6225b8b3fbfafd5
Sarah Parker <sarahparker@google.com>
2021-08-27 17:50:05 -0700

    Fix undefined behaviour in NRS
    Members of mode_threshold were not properly initialized,
    causing variations in bdrate results.
    
    Change-Id: I20cbd91f10832011adee901c0e30bb4a941664a1

--- av1/encoder/rdopt.c
+++ av1/encoder/rdopt.c
@@ -4793,12 +4793,13 @@ static AOM_INLINE void init_inter_mode_search_state(
          sizeof(search_state->dist_order_refs));
 
 #if CONFIG_NEW_REF_SIGNALING
-  search_state->mode_threshold[NEARESTMV] = 0;
   const int *const rd_threshes = cpi->rd.threshes[segment_id][bsize];
-  for (int i = NEARESTMV + 1; i < MB_MODE_COUNT; ++i)
+  for (int i = 0; i < MB_MODE_COUNT; ++i)
     search_state->mode_threshold[i] =
-        ((int64_t)rd_threshes[i] * x->thresh_freq_fact[bsize][i]) >>
-        RD_THRESH_FAC_FRAC_BITS;
+        (i == NEARESTMV)
+            ? 0
+            : ((int64_t)rd_threshes[i] * x->thresh_freq_fact[bsize][i]) >>
+                  RD_THRESH_FAC_FRAC_BITS;
 #else
   for (int i = 0; i <= LAST_NEW_MV_INDEX; ++i)
     search_state->mode_threshold[i] = 0;
