c84115434e27a3ead27150107cb964b0a993a09c
Wan-Teh Chang <wtc@google.com>
2023-02-27 13:43:19 -0800

    test.cmake: Exclude _RC_ from libaom_test_srcs.txt
    Fix a subtle change of behavior in
    commit d601d5a1f01410e39e6ee84dc08fb75d6249c32b.
    
    Before that commit, AOM_RC_INTERFACE_SOURCES contained the source files
    for test_aom_rc_interface and AV1_RC_QMODE_SOURCES contained the source
    files for test_av1_rc_qmode.
    
    That commit merged test_aom_rc_interface and test_av1_rc_qmode into
    test_aom_rc. Following the naming convension, the variable that contains
    the source files for test_aom_rc should be named AOM_RC_SOURCES.
    
    The name of the AOM_RC_TEST_SOURCES variable contains _TEST_, so its
    contents are inadvertently written to the libaom_test_srcs.txt output
    file. Extend the check to exclude variables whose names contain _RC_
    from libaom_test_srcs.txt.
    
    Change-Id: Id3048b0798023453fcc822705ecdc1c7a30034ea

--- test/test.cmake
+++ test/test.cmake
@@ -581,7 +581,7 @@ function(setup_aom_test_targets)
     # cmake-format: off
     if (("${var}" MATCHES "_TEST_" AND NOT
          "${var}" MATCHES
-         "_DATA_\|_CMAKE_\|INTRA_PRED\|_COMPILED\|_HOSTING\|_PERF_\|CODER_")
+         "_DATA_\|_CMAKE_\|INTRA_PRED\|_COMPILED\|_HOSTING\|_PERF_\|CODER_\|_RC_")
         OR (CONFIG_AV1_ENCODER AND ENABLE_ENCODE_PERF_TESTS AND
             "${var}" MATCHES "_ENCODE_PERF_TEST_")
         OR (CONFIG_AV1_DECODER AND ENABLE_DECODE_PERF_TESTS AND
