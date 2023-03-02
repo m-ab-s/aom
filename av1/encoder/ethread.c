2ff9db52f082b369d7ec2b50f4a6c7d950c414a4
Wan-Teh Chang <wtc@google.com>
2023-03-02 11:39:19 -0800

    Init entire num_finished_cols for mb_wiener_var MT
    In av1_calc_mb_wiener_var_mt(), initialize the entire
    intra_row_mt_sync->num_finished_cols array. I didn't figure out the
    number of array elements that need to be initialized, so this is a
    brute-force fix.
    
    Fix valgrind "Conditional jump or move depends on uninitialised
    value(s)" and MSan use-of-uninitialized-value errors in
    av1_row_mt_sync_read().
    
    Bug: aomedia:3397
    Change-Id: I74c98671406ef08fc6439a03d78dcad4de74a678

--- av1/encoder/ethread.c
+++ av1/encoder/ethread.c
@@ -2521,7 +2521,7 @@ void av1_calc_mb_wiener_var_mt(AV1_COMP *cpi, int num_workers,
   intra_row_mt_sync->num_threads_working = num_workers;
   intra_row_mt_sync->next_mi_row = 0;
   memset(intra_row_mt_sync->num_finished_cols, -1,
-         sizeof(*intra_row_mt_sync->num_finished_cols) * num_workers);
+         sizeof(*intra_row_mt_sync->num_finished_cols) * mi_rows);
 
   prepare_wiener_var_workers(cpi, cal_mb_wiener_var_hook, num_workers);
   launch_workers(mt_info, num_workers);
