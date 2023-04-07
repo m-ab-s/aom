7d6d9cd66cc0895d8fb489947dd918ce4179dc76
James Zern <jzern@google.com>
2023-04-07 16:50:19 -0700

    remove single output cnn functions
    av1_cnn_predict_img and av1_cnn_predict_img_highbd are unused.
    
    Bug: aomedia:3416
    Change-Id: I4ad2fdc95f04f1ab4378ca8128d34e80b7635a63

--- av1/encoder/cnn.h
+++ av1/encoder/cnn.h
@@ -184,18 +184,6 @@ bool av1_cnn_predict_img_multi_out_highbd(uint16_t **dgd, int width, int height,
                                           const CNN_CONFIG *cnn_config,
                                           const CNN_THREAD_DATA *thread_data,
                                           int bit_depth, CNN_MULTI_OUT *output);
-
-// Prediction functions from set of input image buffers. This function only
-// supports a single output.
-bool av1_cnn_predict_img(uint8_t **dgd, int width, int height, int stride,
-                         const CNN_CONFIG *cnn_config,
-                         const CNN_THREAD_DATA *thread_data, float **output,
-                         int out_stride);
-bool av1_cnn_predict_img_highbd(uint16_t **dgd, int width, int height,
-                                int stride, const CNN_CONFIG *cnn_config,
-                                const CNN_THREAD_DATA *thread_data,
-                                int bit_depth, float **output, int out_stride);
-
 #ifdef __cplusplus
 }  // extern "C"
 #endif
