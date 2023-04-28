6d461a343105ac04fce4f25fb5285a1013b29359
Salome Thirot <salome.thirot@arm.com>
2023-04-28 14:29:32 +0100

    Add Neon implementation of masked sub-pixel variance
    Add Neon implementation of masked sub-pixel variance functions, as well
    as the corresponding tests.
    
    Change-Id: I91504129c7f2526ea056605eab0fe0dffecb925b

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -1467,7 +1467,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
   foreach (@encoder_block_sizes) {
     ($w, $h) = @$_;
     add_proto qw/unsigned int/, "aom_masked_sub_pixel_variance${w}x${h}", "const uint8_t *src, int src_stride, int xoffset, int yoffset, const uint8_t *ref, int ref_stride, const uint8_t *second_pred, const uint8_t *msk, int msk_stride, int invert_mask, unsigned int *sse";
-    specialize "aom_masked_sub_pixel_variance${w}x${h}", qw/ssse3/;
+    specialize "aom_masked_sub_pixel_variance${w}x${h}", qw/ssse3 neon/;
   }
 
   if (aom_config("CONFIG_AV1_HIGHBITDEPTH") eq "yes") {
