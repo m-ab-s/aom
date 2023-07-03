a2370c9a3a999c2ee6b5e1a9bfc7e58a06ae6c83
Gerda Zsejke More <gerdazsejke.more@arm.com>
2023-07-03 10:52:51 +0200

    Rename highbd_sadxd_neon.c in prep. for adding SAD3D code
    Rename highbd_sad4d_neon.c to highbd_sadxd_neon.c in preparation for
    adding high bitdepth SAD3D Neon paths in a subsequent patch.
    
    Change-Id: I7d58fa4f51fdab956b552211bc07a51a4b0a39c7

--- aom_dsp/aom_dsp.cmake
+++ aom_dsp/aom_dsp.cmake
@@ -298,7 +298,7 @@ if(CONFIG_AV1_ENCODER)
                 "${AOM_ROOT}/aom_dsp/arm/highbd_masked_sad_neon.c"
                 "${AOM_ROOT}/aom_dsp/arm/highbd_quantize_neon.c"
                 "${AOM_ROOT}/aom_dsp/arm/highbd_sad_neon.c"
-                "${AOM_ROOT}/aom_dsp/arm/highbd_sad4d_neon.c"
+                "${AOM_ROOT}/aom_dsp/arm/highbd_sadxd_neon.c"
                 "${AOM_ROOT}/aom_dsp/arm/highbd_subpel_variance_neon.c"
                 "${AOM_ROOT}/aom_dsp/arm/highbd_variance_neon.c")
   endif()
