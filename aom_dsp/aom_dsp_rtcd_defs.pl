69dd20095127ed0bbca156ed99496f1d17233980
Gerda Zsejke More <gerdazsejke.more@arm.com>
2023-07-06 14:22:35 +0200

    Add Neon implementation of HBD OBMC SAD
    Add Neon implementation of aom_highbd_obmc_sad<w>x<h> functions, as
    well as the corresponding tests.
    
    Change-Id: I9f2cc54e9734425cc75f42955e63cc0a44da7bc9

--- aom_dsp/aom_dsp_rtcd_defs.pl
+++ aom_dsp/aom_dsp_rtcd_defs.pl
@@ -1032,7 +1032,7 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
         ($w, $h) = @$_;
         add_proto qw/unsigned int/, "aom_highbd_obmc_sad${w}x${h}", "const uint8_t *pre, int pre_stride, const int32_t *wsrc, const int32_t *mask";
         if (! (($w == 128 && $h == 32) || ($w == 32 && $h == 128))) {
-          specialize "aom_highbd_obmc_sad${w}x${h}", qw/sse4_1 avx2/;
+          specialize "aom_highbd_obmc_sad${w}x${h}", qw/sse4_1 avx2 neon/;
         }
       }
     }
