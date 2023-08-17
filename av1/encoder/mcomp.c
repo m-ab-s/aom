beda1fcf1e82a8fdb271e2b96acef809e5abc5be
Marco Paniconi <marpan@google.com>
2023-08-17 10:23:33 -0700

    rtc: Fix to free buffers in int_pro_motion
    Bug introduced in this CL:
    https://aomedia-review.googlesource.com/c/aom/+/179523
    
    Fixes memory leak for high bit_depth, tested with aomenc
    with 10bit input.
    
    Most likely same issue as nightly test failure.
    
    Change-Id: I46a014ad23a002d2fd4ed52caad0ed5a622c653f

--- av1/encoder/mcomp.c
+++ av1/encoder/mcomp.c
@@ -2097,16 +2097,6 @@ unsigned int av1_int_pro_motion_estimation(const AV1_COMP *cpi, MACROBLOCK *x,
   }
   const int search_width = bw << search_scale;
   const int search_height = bh << search_scale;
-  int16_t *hbuf;
-  int16_t *vbuf;
-  int16_t *src_hbuf;
-  int16_t *src_vbuf;
-  CHECK_MEM_ERROR(cm, hbuf,
-                  (int16_t *)aom_malloc(search_width * sizeof(*hbuf)));
-  CHECK_MEM_ERROR(cm, vbuf,
-                  (int16_t *)aom_malloc(search_height * sizeof(*vbuf)));
-  CHECK_MEM_ERROR(cm, src_hbuf, (int16_t *)aom_malloc(bw * sizeof(*src_hbuf)));
-  CHECK_MEM_ERROR(cm, src_vbuf, (int16_t *)aom_malloc(bh * sizeof(*src_vbuf)));
   const int src_stride = x->plane[0].src.stride;
   const int ref_stride = xd->plane[0].pre[0].stride;
   uint8_t const *ref_buf, *src_buf;
@@ -2145,6 +2135,17 @@ unsigned int av1_int_pro_motion_estimation(const AV1_COMP *cpi, MACROBLOCK *x,
     }
     return best_sad;
   }
+
+  int16_t *hbuf;
+  int16_t *vbuf;
+  int16_t *src_hbuf;
+  int16_t *src_vbuf;
+  CHECK_MEM_ERROR(cm, hbuf,
+                  (int16_t *)aom_malloc(search_width * sizeof(*hbuf)));
+  CHECK_MEM_ERROR(cm, vbuf,
+                  (int16_t *)aom_malloc(search_height * sizeof(*vbuf)));
+  CHECK_MEM_ERROR(cm, src_hbuf, (int16_t *)aom_malloc(bw * sizeof(*src_hbuf)));
+  CHECK_MEM_ERROR(cm, src_vbuf, (int16_t *)aom_malloc(bh * sizeof(*src_vbuf)));
 
   // Set up prediction 1-D reference set for rows.
   ref_buf = xd->plane[0].pre[0].buf - ((search_scale - 1) * bw + (bw >> 1));
