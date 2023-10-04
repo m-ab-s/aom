1ea6901cd0ad8fdab08e0c81786bfb4729b3ff45
Marco Paniconi <marpan@google.com>
2023-10-04 09:55:40 -0700

    rtc: Disable two features for high bitdepth mode
    As quick fix to the issue below: disable accurate_bit_estimate
    and use_model_yrd_large for cpi->oxcf.use_highbitdepth = 1.
    
    Test case that triggers failures is here:
    https://aomedia-review.googlesource.com/c/aom/+/181968
    
    Bug: b/303023614
    Change-Id: I7484f049128e846f44b3a647a9486c9da3d72e67

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1385,10 +1385,9 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     if (speed == 7) {
       sf->rt_sf.prefer_large_partition_blocks = 1;
       // Enable this feature for [360p, 720p] resolution range initially.
-      // Only enable for low bitdepth: see issue: b/303023614.
+      // Only enable for low bitdepth to mitigate issue: b/303023614.
       if (!cpi->rc.rtc_external_ratectrl &&
-          AOMMIN(cm->width, cm->height) <= 720 &&
-          cm->seq_params->bit_depth == 8)
+          AOMMIN(cm->width, cm->height) <= 720 && !cpi->oxcf.use_highbitdepth)
         sf->hl_sf.accurate_bit_estimate = cpi->oxcf.q_cfg.aq_mode == NO_AQ;
     }
     if (speed >= 7) {
