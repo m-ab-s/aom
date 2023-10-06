f33778331224189e2e8938bd52fc1a3e5ffe3a3b
George Steed <george.steed@arm.com>
2023-10-06 09:53:42 +0100

    Use vector additions for shifts by one in shift_neon.h
    Addition instructions have slightly better performance compared to shift
    instructions on some micro-architectures, so use these for shifts by
    one:
    
    * (a << 1) => (a + a)
    * (a >> 1) => (a + 0) >> 1
    
    Change-Id: I28ca05714bf61c6050767f2d5a36319604dd15bd

--- av1/encoder/arm/neon/shift_neon.h
+++ av1/encoder/arm/neon/shift_neon.h
@@ -16,27 +16,33 @@
 
 #include "aom/aom_integer.h"  // For AOM_INLINE.
 
-#define SHIFT_LOOP_HELPER(name, type, intrinsic, shift)              \
+#define SHIFT_LOOP_HELPER(name, type, intrinsic, arg)                \
   static AOM_INLINE void name(const type *in, type *out, int size) { \
     int i = 0;                                                       \
     do {                                                             \
-      out[i] = intrinsic(in[i], shift);                              \
+      out[i] = intrinsic(in[i], arg);                                \
     } while (++i < size);                                            \
   }
 
-SHIFT_LOOP_HELPER(shift_right_1_round_s16_x4, int16x4_t, vrshr_n_s16, 1)
-SHIFT_LOOP_HELPER(shift_right_1_round_s16_x8, int16x8_t, vrshrq_n_s16, 1)
-SHIFT_LOOP_HELPER(shift_right_2_round_s16_x8, int16x8_t, vrshrq_n_s16, 2)
-SHIFT_LOOP_HELPER(shift_right_4_round_s16_x8, int16x8_t, vrshrq_n_s16, 4)
-SHIFT_LOOP_HELPER(shift_left_1_s16_x4, int16x4_t, vshl_n_s16, 1)
-SHIFT_LOOP_HELPER(shift_left_1_s16_x8, int16x8_t, vshlq_n_s16, 1)
 SHIFT_LOOP_HELPER(shift_left_2_s16_x4, int16x4_t, vshl_n_s16, 2)
 SHIFT_LOOP_HELPER(shift_left_2_s16_x8, int16x8_t, vshlq_n_s16, 2)
-
-SHIFT_LOOP_HELPER(shift_right_1_round_s32_x4, int32x4_t, vrshrq_n_s32, 1)
-SHIFT_LOOP_HELPER(shift_right_2_round_s32_x4, int32x4_t, vrshrq_n_s32, 2)
-SHIFT_LOOP_HELPER(shift_right_4_round_s32_x4, int32x4_t, vrshrq_n_s32, 4)
 SHIFT_LOOP_HELPER(shift_left_2_s32_x4, int32x4_t, vshlq_n_s32, 2)
+SHIFT_LOOP_HELPER(shift_right_2_round_s16_x8, int16x8_t, vrshrq_n_s16, 2)
+SHIFT_LOOP_HELPER(shift_right_2_round_s32_x4, int32x4_t, vrshrq_n_s32, 2)
+SHIFT_LOOP_HELPER(shift_right_4_round_s16_x8, int16x8_t, vrshrq_n_s16, 4)
+SHIFT_LOOP_HELPER(shift_right_4_round_s32_x4, int32x4_t, vrshrq_n_s32, 4)
+
+// Addition instructions have slightly better performance compared to shift
+// instructions on some micro-architectures, so use these for shifts by one.
+
+SHIFT_LOOP_HELPER(shift_left_1_s16_x4, int16x4_t, vadd_s16, in[i])
+SHIFT_LOOP_HELPER(shift_left_1_s16_x8, int16x8_t, vaddq_s16, in[i])
+SHIFT_LOOP_HELPER(shift_right_1_round_s16_x4, int16x4_t, vrhadd_s16,
+                  vdup_n_s16(0))
+SHIFT_LOOP_HELPER(shift_right_1_round_s16_x8, int16x8_t, vrhaddq_s16,
+                  vdupq_n_s16(0))
+SHIFT_LOOP_HELPER(shift_right_1_round_s32_x4, int32x4_t, vrhaddq_s32,
+                  vdupq_n_s32(0))
 
 #undef SHIFT_LOOP_HELPER
 
