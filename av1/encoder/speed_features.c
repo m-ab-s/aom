6288d8f3e6bc0f63b12e57b6f944f5b4b8ee6c3a
Marco Paniconi <marpan@google.com>
2023-10-09 09:51:47 -0700

    rtc: Disable speed feature high bitdepth mode
    Disable the estimate_motion_for_var_based_partition for
    cpi->oxcf.use_highbitdepth = 1.
    
    Bug: b/303023614
    Change-Id: I4bc1c48637391e66d306219aa7831de6121409cb
    (cherry picked from commit 85f1a09cc686dc4fdf6863ef5f25718fe5d5db34)

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -1578,6 +1578,10 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
     // can be removed once it's fixed for lossless mode.
     sf->hl_sf.accurate_bit_estimate = 0;
   }
+  if (cpi->oxcf.use_highbitdepth) {
+    // Disable for use_highbitdepth = 1 to mitigate issue: b/303023614.
+    sf->rt_sf.estimate_motion_for_var_based_partition = 0;
+  }
 }
 
 // TODO(kyslov): now this is very similar to
