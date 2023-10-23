eccfc281963be34c24d1449d5faabda45d4c85fe
Salome Thirot <salome.thirot@arm.com>
2023-10-23 15:42:56 +0100

    Add explicit Neon implementation of cdef_find_dir
    Most SIMD accelerated functions have their own architecture-specific
    implementation, but cdef functions are implemented using an architecture
    agnostic intrinsics layer. Since architecture-agnostic implementations
    are seldom optimal, we would like to write an explicit Neon
    implementation of CDEF. This patch is the first step: copying the
    implementation as-is and substituting the architecture agnostic
    intrinsics for Neon ACLE functions, without any attempt to simplify or
    optimize the code. Refactoring and optimization will take place in
    subsequent patches.
    
    Change-Id: I04bed9a55cde63a8c600190e60b025f58c5e76e7

--- av1/common/arm/cdef_block_neon.c
+++ av1/common/arm/cdef_block_neon.c
@@ -28,6 +28,341 @@ void cdef_copy_rect8_8bit_to_16bit_neon(uint16_t *dst, int dstride,
   }
 }
 
+static INLINE int16x8_t v128_from_64_neon(int64_t a, int64_t b) {
+  return vreinterpretq_s16_s64(vcombine_s64(vcreate_s64(a), vcreate_s64(b)));
+}
+
+#define SHL_HIGH_NEON(n)                                                   \
+  static INLINE int16x8_t v128_shl_##n##_byte_neon(int16x8_t a) {          \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                            \
+    return v128_from_64_neon(                                              \
+        0, (uint64_t)vshl_n_u64(vreinterpret_u64_s64(vget_low_s64(a_s64)), \
+                                (n - 8) * 8));                             \
+  }
+
+#define SHL_NEON(n)                                               \
+  static INLINE int16x8_t v128_shl_##n##_byte_neon(int16x8_t a) { \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                   \
+    return v128_from_64_neon(                                     \
+        0, (uint64_t)vreinterpret_u64_s64(vget_low_s64(a_s64)));  \
+  }
+
+#define SHL_LOW_NEON(n)                                                    \
+  static INLINE int16x8_t v128_shl_##n##_byte_neon(int16x8_t a) {          \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                            \
+    return v128_from_64_neon(                                              \
+        (uint64_t)vshl_n_u64(vreinterpret_u64_s64(vget_low_s64(a_s64)),    \
+                             n * 8),                                       \
+        (uint64_t)vorr_u64(                                                \
+            vshl_n_u64(vreinterpret_u64_s64(vget_high_s64(a_s64)), n * 8), \
+            vshr_n_u64(vreinterpret_u64_s64(vget_low_s64(a_s64)),          \
+                       (8 - n) * 8)));                                     \
+  }
+
+SHL_HIGH_NEON(14)
+SHL_HIGH_NEON(12)
+SHL_HIGH_NEON(10)
+SHL_NEON(8)
+SHL_LOW_NEON(6)
+SHL_LOW_NEON(4)
+SHL_LOW_NEON(2)
+
+#define v128_shl_n_byte_neon(a, n) v128_shl_##n##_byte_neon(a)
+
+#define SHR_HIGH_NEON(n)                                                 \
+  static INLINE int16x8_t v128_shr_##n##_byte_neon(int16x8_t a) {        \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                          \
+    return v128_from_64_neon(                                            \
+        (uint64_t)vshr_n_u64(vreinterpret_u64_s64(vget_high_s64(a_s64)), \
+                             (n - 8) * 8),                               \
+        0);                                                              \
+  }
+
+#define SHR_NEON(n)                                               \
+  static INLINE int16x8_t v128_shr_##n##_byte_neon(int16x8_t a) { \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                   \
+    return v128_from_64_neon(                                     \
+        (uint64_t)vreinterpret_u64_s64(vget_high_s64(a_s64)), 0); \
+  }
+
+#define SHR_LOW_NEON(n)                                                   \
+  static INLINE int16x8_t v128_shr_##n##_byte_neon(int16x8_t a) {         \
+    int64x2_t a_s64 = vreinterpretq_s64_s16(a);                           \
+    return v128_from_64_neon(                                             \
+        (uint64_t)vorr_u64(                                               \
+            vshr_n_u64(vreinterpret_u64_s64(vget_low_s64(a_s64)), n * 8), \
+            vshl_n_u64(vreinterpret_u64_s64(vget_high_s64(a_s64)),        \
+                       (8 - n) * 8)),                                     \
+        (uint64_t)vshr_n_u64(vreinterpret_u64_s64(vget_high_s64(a_s64)),  \
+                             n * 8));                                     \
+  }
+
+SHR_HIGH_NEON(14)
+SHR_HIGH_NEON(12)
+SHR_HIGH_NEON(10)
+SHR_NEON(8)
+SHR_LOW_NEON(6)
+SHR_LOW_NEON(4)
+SHR_LOW_NEON(2)
+
+#define v128_shr_n_byte_neon(a, n) v128_shr_##n##_byte_neon(a)
+
+static INLINE uint32x4_t v128_madd_s16_neon(int16x8_t a, int16x8_t b) {
+  uint32x4_t t1 =
+      vreinterpretq_u32_s32(vmull_s16(vget_low_s16(a), vget_low_s16(b)));
+  uint32x4_t t2 =
+      vreinterpretq_u32_s32(vmull_s16(vget_high_s16(a), vget_high_s16(b)));
+#if AOM_ARCH_AARCH64
+  return vpaddq_u32(t1, t2);
+#else
+  return vcombine_u32(vpadd_u32(vget_low_u32(t1), vget_high_u32(t1)),
+                      vpadd_u32(vget_low_u32(t2), vget_high_u32(t2)));
+#endif
+}
+
+// partial A is a 16-bit vector of the form:
+// [x8 x7 x6 x5 x4 x3 x2 x1] and partial B has the form:
+// [0  y1 y2 y3 y4 y5 y6 y7].
+// This function computes (x1^2+y1^2)*C1 + (x2^2+y2^2)*C2 + ...
+// (x7^2+y2^7)*C7 + (x8^2+0^2)*C8 where the C1..C8 constants are in const1
+// and const2.
+static INLINE uint32x4_t fold_mul_and_sum_neon(int16x8_t partiala,
+                                               int16x8_t partialb,
+                                               uint32x4_t const1,
+                                               uint32x4_t const2) {
+  int16x8_t tmp;
+  // Reverse partial B.
+  uint8x16_t pattern = vreinterpretq_u8_u64(
+      vcombine_u64(vcreate_u64((uint64_t)0x07060908 << 32 | 0x0b0a0d0c),
+                   vcreate_u64((uint64_t)0x0f0e0100 << 32 | 0x03020504)));
+
+#if AOM_ARCH_AARCH64
+  partialb =
+      vreinterpretq_s16_s8(vqtbl1q_s8(vreinterpretq_s8_s16(partialb), pattern));
+#else
+  int8x8x2_t p = { { vget_low_s8(vreinterpretq_s8_s16(partialb)),
+                     vget_high_s8(vreinterpretq_s8_s16(partialb)) } };
+  int8x8_t shuffle_hi = vtbl2_s8(p, vget_high_s8(vreinterpretq_s8_u8(pattern)));
+  int8x8_t shuffle_lo = vtbl2_s8(p, vget_low_s8(vreinterpretq_s8_u8(pattern)));
+  partialb = vreinterpretq_s16_s8(vcombine_s8(shuffle_lo, shuffle_hi));
+#endif
+
+  // Interleave the x and y values of identical indices and pair x8 with 0.
+  tmp = partiala;
+  partiala = vzipq_s16(partiala, partialb).val[0];
+  partialb = vzipq_s16(tmp, partialb).val[1];
+  // Square and add the corresponding x and y values.
+  uint32x4_t partiala_u32 = v128_madd_s16_neon(partiala, partiala);
+  uint32x4_t partialb_u32 = v128_madd_s16_neon(partialb, partialb);
+
+  // Multiply by constant.
+  partiala_u32 = vmulq_u32(partiala_u32, const1);
+  partialb_u32 = vmulq_u32(partialb_u32, const2);
+
+  // Sum all results.
+  partiala_u32 = vaddq_u32(partiala_u32, partialb_u32);
+  return partiala_u32;
+}
+
+static INLINE uint64x2_t ziplo_u64(uint32x4_t a, uint32x4_t b) {
+  return vcombine_u64(vget_low_u64(vreinterpretq_u64_u32(a)),
+                      vget_low_u64(vreinterpretq_u64_u32(b)));
+}
+
+static INLINE uint64x2_t ziphi_u64(uint32x4_t a, uint32x4_t b) {
+  return vcombine_u64(vget_high_u64(vreinterpretq_u64_u32(a)),
+                      vget_high_u64(vreinterpretq_u64_u32(b)));
+}
+
+static INLINE uint32x4_t hsum4_neon(uint32x4_t x0, uint32x4_t x1, uint32x4_t x2,
+                                    uint32x4_t x3) {
+  uint32x4_t t0, t1, t2, t3;
+  t0 = vzipq_u32(x0, x1).val[0];
+  t1 = vzipq_u32(x2, x3).val[0];
+  t2 = vzipq_u32(x0, x1).val[1];
+  t3 = vzipq_u32(x2, x3).val[1];
+  x0 = vreinterpretq_u32_u64(ziplo_u64(t0, t1));
+  x1 = vreinterpretq_u32_u64(ziphi_u64(t0, t1));
+  x2 = vreinterpretq_u32_u64(ziplo_u64(t2, t3));
+  x3 = vreinterpretq_u32_u64(ziphi_u64(t2, t3));
+  return vaddq_u32(vaddq_u32(x0, x1), vaddq_u32(x2, x3));
+}
+
+static INLINE uint32x4_t compute_directions_neon(int16x8_t lines[8],
+                                                 uint32_t cost[4]) {
+  int16x8_t partial4a, partial4b, partial5a, partial5b, partial6, partial7a,
+      partial7b;
+  int16x8_t tmp;
+
+  // Partial sums for lines 0 and 1.
+  partial4a = v128_shl_n_byte_neon(lines[0], 14);
+  partial4b = v128_shr_n_byte_neon(lines[0], 2);
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[1], 12));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[1], 4));
+  tmp = vaddq_s16(lines[0], lines[1]);
+  partial5a = v128_shl_n_byte_neon(tmp, 10);
+  partial5b = v128_shr_n_byte_neon(tmp, 6);
+  partial7a = v128_shl_n_byte_neon(tmp, 4);
+  partial7b = v128_shr_n_byte_neon(tmp, 12);
+  partial6 = tmp;
+
+  // Partial sums for lines 2 and 3.
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[2], 10));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[2], 6));
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[3], 8));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[3], 8));
+  tmp = vaddq_s16(lines[2], lines[3]);
+  partial5a = vaddq_s16(partial5a, v128_shl_n_byte_neon(tmp, 8));
+  partial5b = vaddq_s16(partial5b, v128_shr_n_byte_neon(tmp, 8));
+  partial7a = vaddq_s16(partial7a, v128_shl_n_byte_neon(tmp, 6));
+  partial7b = vaddq_s16(partial7b, v128_shr_n_byte_neon(tmp, 10));
+  partial6 = vaddq_s16(partial6, tmp);
+
+  // Partial sums for lines 4 and 5.
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[4], 6));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[4], 10));
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[5], 4));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[5], 12));
+  tmp = vaddq_s16(lines[4], lines[5]);
+  partial5a = vaddq_s16(partial5a, v128_shl_n_byte_neon(tmp, 6));
+  partial5b = vaddq_s16(partial5b, v128_shr_n_byte_neon(tmp, 10));
+  partial7a = vaddq_s16(partial7a, v128_shl_n_byte_neon(tmp, 8));
+  partial7b = vaddq_s16(partial7b, v128_shr_n_byte_neon(tmp, 8));
+  partial6 = vaddq_s16(partial6, tmp);
+
+  // Partial sums for lines 6 and 7.
+  partial4a = vaddq_s16(partial4a, v128_shl_n_byte_neon(lines[6], 2));
+  partial4b = vaddq_s16(partial4b, v128_shr_n_byte_neon(lines[6], 14));
+  partial4a = vaddq_s16(partial4a, lines[7]);
+  tmp = vaddq_s16(lines[6], lines[7]);
+  partial5a = vaddq_s16(partial5a, v128_shl_n_byte_neon(tmp, 4));
+  partial5b = vaddq_s16(partial5b, v128_shr_n_byte_neon(tmp, 12));
+  partial7a = vaddq_s16(partial7a, v128_shl_n_byte_neon(tmp, 10));
+  partial7b = vaddq_s16(partial7b, v128_shr_n_byte_neon(tmp, 6));
+  partial6 = vaddq_s16(partial6, tmp);
+
+  uint32x4_t const0 = vreinterpretq_u32_u64(
+      vcombine_u64(vcreate_u64((uint64_t)420 << 32 | 840),
+                   vcreate_u64((uint64_t)210 << 32 | 280)));
+  uint32x4_t const1 = vreinterpretq_u32_u64(
+      vcombine_u64(vcreate_u64((uint64_t)140 << 32 | 168),
+                   vcreate_u64((uint64_t)105 << 32 | 120)));
+  uint32x4_t const2 = vreinterpretq_u32_u64(
+      vcombine_u64(vcreate_u64(0), vcreate_u64((uint64_t)210 << 32 | 420)));
+  uint32x4_t const3 = vreinterpretq_u32_u64(
+      vcombine_u64(vcreate_u64((uint64_t)105 << 32 | 140),
+                   vcreate_u64((uint64_t)105 << 32 | 105)));
+
+  // Compute costs in terms of partial sums.
+  uint32x4_t partial4a_u32 =
+      fold_mul_and_sum_neon(partial4a, partial4b, const0, const1);
+  uint32x4_t partial7a_u32 =
+      fold_mul_and_sum_neon(partial7a, partial7b, const2, const3);
+  uint32x4_t partial5a_u32 =
+      fold_mul_and_sum_neon(partial5a, partial5b, const2, const3);
+  uint32x4_t partial6_u32 = v128_madd_s16_neon(partial6, partial6);
+  partial6_u32 = vmulq_u32(partial6_u32, vdupq_n_u32(105));
+
+  partial4a_u32 =
+      hsum4_neon(partial4a_u32, partial5a_u32, partial6_u32, partial7a_u32);
+  vst1q_u32(cost, partial4a_u32);
+  return partial4a_u32;
+}
+
+static INLINE int64x2_t ziplo_s64(int32x4_t a, int32x4_t b) {
+  return vcombine_s64(vget_low_s64(vreinterpretq_s64_s32(a)),
+                      vget_low_s64(vreinterpretq_s64_s32(b)));
+}
+
+static INLINE int64x2_t ziphi_s64(int32x4_t a, int32x4_t b) {
+  return vcombine_s64(vget_high_s64(vreinterpretq_s64_s32(a)),
+                      vget_high_s64(vreinterpretq_s64_s32(b)));
+}
+
+// Transpose and reverse the order of the lines -- equivalent to a 90-degree
+// counter-clockwise rotation of the pixels.
+static INLINE void array_reverse_transpose_8x8_neon(int16x8_t *in,
+                                                    int16x8_t *res) {
+  const int32x4_t tr0_0 = vreinterpretq_s32_s16(vzipq_s16(in[0], in[1]).val[0]);
+  const int32x4_t tr0_1 = vreinterpretq_s32_s16(vzipq_s16(in[2], in[3]).val[0]);
+  const int32x4_t tr0_2 = vreinterpretq_s32_s16(vzipq_s16(in[0], in[1]).val[1]);
+  const int32x4_t tr0_3 = vreinterpretq_s32_s16(vzipq_s16(in[2], in[3]).val[1]);
+  const int32x4_t tr0_4 = vreinterpretq_s32_s16(vzipq_s16(in[4], in[5]).val[0]);
+  const int32x4_t tr0_5 = vreinterpretq_s32_s16(vzipq_s16(in[6], in[7]).val[0]);
+  const int32x4_t tr0_6 = vreinterpretq_s32_s16(vzipq_s16(in[4], in[5]).val[1]);
+  const int32x4_t tr0_7 = vreinterpretq_s32_s16(vzipq_s16(in[6], in[7]).val[1]);
+
+  const int32x4_t tr1_0 = vzipq_s32(tr0_0, tr0_1).val[0];
+  const int32x4_t tr1_1 = vzipq_s32(tr0_4, tr0_5).val[0];
+  const int32x4_t tr1_2 = vzipq_s32(tr0_0, tr0_1).val[1];
+  const int32x4_t tr1_3 = vzipq_s32(tr0_4, tr0_5).val[1];
+  const int32x4_t tr1_4 = vzipq_s32(tr0_2, tr0_3).val[0];
+  const int32x4_t tr1_5 = vzipq_s32(tr0_6, tr0_7).val[0];
+  const int32x4_t tr1_6 = vzipq_s32(tr0_2, tr0_3).val[1];
+  const int32x4_t tr1_7 = vzipq_s32(tr0_6, tr0_7).val[1];
+
+  res[7] = vreinterpretq_s16_s64(ziplo_s64(tr1_0, tr1_1));
+  res[6] = vreinterpretq_s16_s64(ziphi_s64(tr1_0, tr1_1));
+  res[5] = vreinterpretq_s16_s64(ziplo_s64(tr1_2, tr1_3));
+  res[4] = vreinterpretq_s16_s64(ziphi_s64(tr1_2, tr1_3));
+  res[3] = vreinterpretq_s16_s64(ziplo_s64(tr1_4, tr1_5));
+  res[2] = vreinterpretq_s16_s64(ziphi_s64(tr1_4, tr1_5));
+  res[1] = vreinterpretq_s16_s64(ziplo_s64(tr1_6, tr1_7));
+  res[0] = vreinterpretq_s16_s64(ziphi_s64(tr1_6, tr1_7));
+}
+
+static INLINE uint32_t compute_best_dir(uint8x16_t a) {
+  uint8x16_t idx =
+      vandq_u8(a, vreinterpretq_u8_u64(vdupq_n_u64(0x8040201008040201ULL)));
+#if AOM_ARCH_AARCH64
+  return vaddv_u8(vget_low_u8(idx)) + (vaddv_u8(vget_high_u8(idx)) << 8);
+#else
+  uint64x2_t m = vpaddlq_u32(vpaddlq_u16(vpaddlq_u8(idx)));
+  uint8x16_t s = vreinterpretq_u8_u64(m);
+  return vget_lane_u32(
+      vreinterpret_u32_u8(vzip_u8(vget_low_u8(s), vget_high_u8(s)).val[0]), 0);
+#endif
+}
+
+int cdef_find_dir_neon(const uint16_t *img, int stride, int32_t *var,
+                       int coeff_shift) {
+  uint32_t cost[8];
+  uint32_t best_cost = 0;
+  int best_dir = 0;
+  int16x8_t lines[8];
+  for (int i = 0; i < 8; i++) {
+    uint16x8_t s = vld1q_u16(&img[i * stride]);
+    lines[i] = vreinterpretq_s16_u16(
+        vsubq_u16(vshlq_u16(s, vdupq_n_s16(-coeff_shift)), vdupq_n_u16(128)));
+  }
+
+  // Compute "mostly vertical" directions.
+  uint32x4_t cost47 = compute_directions_neon(lines, cost + 4);
+
+  array_reverse_transpose_8x8_neon(lines, lines);
+
+  // Compute "mostly horizontal" directions.
+  uint32x4_t cost03 = compute_directions_neon(lines, cost);
+
+  uint32x4_t max_cost = vmaxq_u32(cost03, cost47);
+  max_cost = vmaxq_u32(max_cost, vextq_u32(max_cost, max_cost, 2));
+  max_cost = vmaxq_u32(max_cost, vextq_u32(max_cost, max_cost, 1));
+  best_cost = vgetq_lane_u32(max_cost, 0);
+  uint16x8_t idx = vcombine_u16(vqmovn_u32(vceqq_u32(max_cost, cost03)),
+                                vqmovn_u32(vceqq_u32(max_cost, cost47)));
+  uint8x16_t idx_u8 = vcombine_u8(vqmovn_u16(idx), vqmovn_u16(idx));
+  best_dir = compute_best_dir(idx_u8);
+  best_dir = get_msb(best_dir ^ (best_dir - 1));  // Count trailing zeros
+
+  // Difference between the optimal variance and the variance along the
+  // orthogonal direction. Again, the sum(x^2) terms cancel out.
+  *var = best_cost - cost[(best_dir + 4) & 7];
+  // We'd normally divide by 840, but dividing by 1024 is close enough
+  // for what we're going to do with this.
+  *var >>= 10;
+  return best_dir;
+}
+
 void cdef_find_dir_dual_neon(const uint16_t *img1, const uint16_t *img2,
                              int stride, int32_t *var_out_1st,
                              int32_t *var_out_2nd, int coeff_shift,
