f969060afe8cd567133462b491238815d25c0359
Marco Paniconi <marpan@google.com>
2024-04-08 13:53:02 -0700

    rtc-speed 11: Condition skip_cdef on source_variance
    For speed 11 video mode, resoln <= VGA:
    keep skip_cdef = 1 for speed 11, but condition the skip
    logic on the block spatial variance. This improves visual
    quality near low texture areas, like faces. To compensate
    for the speed loss skip testing the NEARMV mode.
    
    Update the comments on skip_cdef.
    
    stats change for speed 11: avg/ovr/ssim, IC speedup:
    rtc_derf: -3.86/-3.80/-6.06, -1.43
    rtc (vga only): -3.06/-2.70/-5.79, -1.11
    
    Change-Id: I1d51bd37c6914b4d9df630f95265171c7a53133a

--- av1/encoder/nonrd_pickmode.c
+++ av1/encoder/nonrd_pickmode.c
@@ -1886,14 +1886,17 @@ static AOM_INLINE int skip_mode_by_low_temp(
 
 static AOM_INLINE int skip_mode_by_bsize_and_ref_frame(
     PREDICTION_MODE mode, MV_REFERENCE_FRAME ref_frame, BLOCK_SIZE bsize,
-    int extra_prune, unsigned int sse_zeromv_norm, int more_prune) {
+    int extra_prune, unsigned int sse_zeromv_norm, int more_prune,
+    int skip_nearmv) {
   const unsigned int thresh_skip_golden = 500;
 
   if (ref_frame != LAST_FRAME && sse_zeromv_norm < thresh_skip_golden &&
       mode == NEWMV)
     return 1;
 
-  if (bsize == BLOCK_128X128 && mode == NEWMV) return 1;
+  if ((bsize == BLOCK_128X128 && mode == NEWMV) ||
+      (skip_nearmv && mode == NEARMV))
+    return 1;
 
   // Skip testing non-LAST if this flag is set.
   if (extra_prune) {
@@ -2478,7 +2481,8 @@ static AOM_FORCE_INLINE bool skip_inter_mode_nonrd(
   // properties.
   if (skip_mode_by_bsize_and_ref_frame(
           *this_mode, *ref_frame, bsize, x->nonrd_prune_ref_frame_search,
-          sse_zeromv_norm, rt_sf->nonrd_aggressive_skip))
+          sse_zeromv_norm, rt_sf->nonrd_aggressive_skip,
+          rt_sf->increase_source_sad_thresh))
     return true;
 
   // Skip mode based on low temporal variance and souce sad.
