c349b1ebd23dce55cf23c5f8fa56d5a9416971b0
Salome Thirot <salome.thirot@arm.com>
2024-04-25 15:16:17 +0100

    Add 4-tap path for av1_convolve_2d_horiz_sr_neon
    Add 4-tap specialization for the horizontal pass of
    av1_convolve_2d_sr_neon. This gives up to 30% uplift over using the
    8-tap path.
    
    Change-Id: I6c9b7be0e90661a36cef95db1daedc2fabd6a31e

--- aom_dsp/arm/mem_neon.h
+++ aom_dsp/arm/mem_neon.h
@@ -654,6 +654,13 @@ static INLINE void store_s16_8x4(int16_t *s, ptrdiff_t dst_stride,
   vst1q_s16(s, s3);
 }
 
+static INLINE void store_s16_8x2(int16_t *s, ptrdiff_t dst_stride,
+                                 const int16x8_t s0, const int16x8_t s1) {
+  vst1q_s16(s, s0);
+  s += dst_stride;
+  vst1q_s16(s, s1);
+}
+
 static INLINE void load_u8_8x11(const uint8_t *s, ptrdiff_t p,
                                 uint8x8_t *const s0, uint8x8_t *const s1,
                                 uint8x8_t *const s2, uint8x8_t *const s3,
@@ -1248,6 +1255,12 @@ static INLINE uint8x8_t load_u8_gather_s16_x8(const uint8_t *src,
     memcpy(dst, &a, 8);                                            \
   } while (0)
 
+#define store_s16_4x1_lane(dst, src, lane)                        \
+  do {                                                            \
+    int64_t a = vgetq_lane_s64(vreinterpretq_s64_s16(src), lane); \
+    memcpy(dst, &a, 8);                                           \
+  } while (0)
+
 // Store the low 16-bits from a single vector.
 static INLINE void store_u8_2x1(uint8_t *dst, const uint8x8_t src) {
   store_u8_2x1_lane(dst, src, 0);
@@ -1307,9 +1320,18 @@ static INLINE void store_u16x4_strided_x2(uint16_t *dst, uint32_t dst_stride,
   store_u16_4x1_lane(dst, src, 1);
 }
 
+// Store two blocks of 64-bits from a single vector.
+static INLINE void store_s16x4_strided_x2(int16_t *dst, int32_t dst_stride,
+                                          int16x8_t src) {
+  store_s16_4x1_lane(dst, src, 0);
+  dst += dst_stride;
+  store_s16_4x1_lane(dst, src, 1);
+}
+
 #undef store_u8_2x1_lane
 #undef store_u8_4x1_lane
 #undef store_u16_2x1_lane
 #undef store_u16_4x1_lane
+#undef store_s16_4x1_lane
 
 #endif  // AOM_AOM_DSP_ARM_MEM_NEON_H_
