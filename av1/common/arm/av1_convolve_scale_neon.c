cd4d6d1d2aebb495891e47b4efa7f178e337a2d8
Wan-Teh Chang <wtc@google.com>
2024-05-30 16:31:30 -0700

    Fix ClangTidy misc-include-cleaner warnings
    Fix the following warnings:
      no header providing "int16_t" is directly included
      no header providing "ROUND0_BITS" is directly included
      no header providing "SCALE_EXTRA_BITS" is directly included
      no header providing "InterpFilterParams" is directly included
      no header providing "MAX_FILTER_TAP" is directly included
      no header providing "UNLIKELY" is directly included
    
    Change-Id: I94bdb705a460f63a3566ef1f0ad7539af0f6dafc

--- av1/common/arm/av1_convolve_scale_neon.c
+++ av1/common/arm/av1_convolve_scale_neon.c
@@ -11,13 +11,18 @@
 
 #include <arm_neon.h>
 #include <assert.h>
+#include <stdint.h>
 
 #include "config/aom_config.h"
 #include "config/av1_rtcd.h"
 
+#include "aom_dsp/aom_dsp_common.h"
+#include "aom_dsp/aom_filter.h"
 #include "aom_dsp/arm/mem_neon.h"
 #include "aom_dsp/arm/transpose_neon.h"
 #include "av1/common/arm/convolve_scale_neon.h"
+#include "av1/common/convolve.h"
+#include "av1/common/filter.h"
 
 static INLINE int16x4_t convolve8_4_h(const int16x4_t s0, const int16x4_t s1,
                                       const int16x4_t s2, const int16x4_t s3,
