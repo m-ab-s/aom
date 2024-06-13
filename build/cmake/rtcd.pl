817973695d9f4ddb757a21c6a8aebacd7eb57d77
James Zern <jzern@google.com>
2024-06-13 11:51:43 -0700

    rtcd.pl: add license header to generated files
    Bug: aomedia:3525
    Change-Id: I614056558fb5439b448342e0c01e53bd8da85585

--- build/cmake/rtcd.pl
+++ build/cmake/rtcd.pl
@@ -219,7 +219,20 @@ sub filter {
 #
 sub common_top() {
   my $include_guard = uc($opts{sym})."_H_";
+  my @time = localtime;
+  my $year = $time[5] + 1900;
   print <<EOF;
+/*
+ * Copyright (c) ${year}, Alliance for Open Media. All rights reserved.
+ *
+ * This source code is subject to the terms of the BSD 2 Clause License and
+ * the Alliance for Open Media Patent License 1.0. If the BSD 2 Clause License
+ * was not distributed with this source code in the LICENSE file, you can
+ * obtain it at www.aomedia.org/license/software. If the Alliance for Open
+ * Media Patent License 1.0 was not distributed with this source code in the
+ * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
+ */
+
 // This file is generated. Do not edit.
 #ifndef ${include_guard}
 #define ${include_guard}
