3211b7fb55f41404aabd41cd0e611750c8d932a0
Gerda Zsejke More <gerdazsejke.more@arm.com>
2024-08-07 09:47:24 +0200

    Add Neon Dotprod 4/1 scale spec. for av1_resize_and_extend_frame
    Add an Armv8.4 DotProd implementation for av1_resize_and_extend_frame
    that specialises on 4 to 1 scaling. When BILINEAR filter is used or
    phase value is equal to 0 the Armv8.0 Neon implementation is called.
    
    Change-Id: Id5213a82338d7c8fb166461880984dfb2bf6544b

--- av1/common/arm/resize_neon.c
+++ av1/common/arm/resize_neon.c
@@ -318,53 +318,6 @@ static inline void scale_4_to_1_horiz_6tap(const uint8_t *src,
   } while (h > 0);
 }
 
-static inline void scale_4_to_1_vert_6tap(const uint8_t *src,
-                                          const int src_stride, int w, int h,
-                                          uint8_t *dst, const int dst_stride,
-                                          const int16x8_t filters) {
-  do {
-    uint8x8_t t0 = vld1_u8(src + 0 * src_stride);
-    uint8x8_t t1 = vld1_u8(src + 1 * src_stride);
-
-    int16x8_t s0 = vreinterpretq_s16_u16(vmovl_u8(t0));
-    int16x8_t s1 = vreinterpretq_s16_u16(vmovl_u8(t1));
-
-    const uint8_t *s = src + 2 * src_stride;
-    uint8_t *d = dst;
-    int height = h;
-
-    do {
-      uint8x8_t t2, t3, t4, t5, t6, t7, t8, t9;
-      load_u8_8x8(s, src_stride, &t2, &t3, &t4, &t5, &t6, &t7, &t8, &t9);
-
-      int16x8_t s2 = vreinterpretq_s16_u16(vmovl_u8(t2));
-      int16x8_t s3 = vreinterpretq_s16_u16(vmovl_u8(t3));
-      int16x8_t s4 = vreinterpretq_s16_u16(vmovl_u8(t4));
-      int16x8_t s5 = vreinterpretq_s16_u16(vmovl_u8(t5));
-      int16x8_t s6 = vreinterpretq_s16_u16(vmovl_u8(t6));
-      int16x8_t s7 = vreinterpretq_s16_u16(vmovl_u8(t7));
-      int16x8_t s8 = vreinterpretq_s16_u16(vmovl_u8(t8));
-      int16x8_t s9 = vreinterpretq_s16_u16(vmovl_u8(t9));
-
-      uint8x8_t d0 = scale_filter6_8(s0, s1, s2, s3, s4, s5, filters);
-      uint8x8_t d1 = scale_filter6_8(s4, s5, s6, s7, s8, s9, filters);
-
-      store_u8_8x2(d, dst_stride, d0, d1);
-
-      s0 = s8;
-      s1 = s9;
-
-      s += 8 * src_stride;
-      d += 2 * dst_stride;
-      height -= 2;
-    } while (height > 0);
-
-    src += 8;
-    dst += 8;
-    w -= 8;
-  } while (w > 0);
-}
-
 static inline void scale_plane_4_to_1_6tap(const uint8_t *src,
                                            const int src_stride, uint8_t *dst,
                                            const int dst_stride, const int w,
--- av1/common/arm/resize_neon_dotprod.c
+++ av1/common/arm/resize_neon_dotprod.c
@@ -24,6 +24,9 @@ DECLARE_ALIGNED(16, static const uint8_t, kScale2DotProdPermuteTbl[32]) = {
   0, 1, 2, 3, 2, 3, 4, 5, 4, 5,  6,  7,  6,  7,  8,  9,
   4, 5, 6, 7, 6, 7, 8, 9, 8, 9, 10, 11, 10, 11, 12, 13
 };
+DECLARE_ALIGNED(16, static const uint8_t, kScale4DotProdPermuteTbl[16]) = {
+  0, 1, 2, 3, 4, 5, 6, 7, 4, 5, 6, 7, 8, 9, 10, 11
+};
 // clang-format on
 
 static inline uint8x8_t scale_2_to_1_filter8_8(const uint8x16_t s0,
@@ -127,11 +130,109 @@ static inline void scale_plane_2_to_1_8tap(const uint8_t *src,
                          filters);
 }
 
+static inline uint8x8_t scale_4_to_1_filter8_8(
+    const uint8x16_t s0, const uint8x16_t s1, const uint8x16_t s2,
+    const uint8x16_t s3, const uint8x16_t permute_tbl, const int8x8_t filter) {
+  int8x16_t filters = vcombine_s8(filter, filter);
+
+  // Transform sample range to [-128, 127] for 8-bit signed dot product.
+  int8x16_t s0_128 = vreinterpretq_s8_u8(vsubq_u8(s0, vdupq_n_u8(128)));
+  int8x16_t s1_128 = vreinterpretq_s8_u8(vsubq_u8(s1, vdupq_n_u8(128)));
+  int8x16_t s2_128 = vreinterpretq_s8_u8(vsubq_u8(s2, vdupq_n_u8(128)));
+  int8x16_t s3_128 = vreinterpretq_s8_u8(vsubq_u8(s3, vdupq_n_u8(128)));
+
+  int8x16_t perm_samples[4] = { vqtbl1q_s8(s0_128, permute_tbl),
+                                vqtbl1q_s8(s1_128, permute_tbl),
+                                vqtbl1q_s8(s2_128, permute_tbl),
+                                vqtbl1q_s8(s3_128, permute_tbl) };
+
+  // Dot product constant:
+  // The shim of 128 << FILTER_BITS is needed because we are subtracting 128
+  // from every source value. The additional right shift by one is needed
+  // because we halved the filter values and will use a pairwise add.
+  const int32x4_t acc = vdupq_n_s32((128 << FILTER_BITS) >> 2);
+
+  int32x4_t sum0 = vdotq_s32(acc, perm_samples[0], filters);
+  int32x4_t sum1 = vdotq_s32(acc, perm_samples[1], filters);
+  int32x4_t sum2 = vdotq_s32(acc, perm_samples[2], filters);
+  int32x4_t sum3 = vdotq_s32(acc, perm_samples[3], filters);
+
+  int32x4_t sum01 = vpaddq_s32(sum0, sum1);
+  int32x4_t sum23 = vpaddq_s32(sum2, sum3);
+
+  int16x8_t sum = vcombine_s16(vmovn_s32(sum01), vmovn_s32(sum23));
+
+  // We halved the filter values so -1 from right shift.
+  return vqrshrun_n_s16(sum, FILTER_BITS - 1);
+}
+
+static inline void scale_4_to_1_horiz_8tap(const uint8_t *src,
+                                           const int src_stride, int w, int h,
+                                           uint8_t *dst, const int dst_stride,
+                                           const int16x8_t filters) {
+  const int8x8_t filter = vmovn_s16(filters);
+  const uint8x16_t permute_tbl = vld1q_u8(kScale4DotProdPermuteTbl);
+
+  do {
+    const uint8_t *s = src;
+    uint8_t *d = dst;
+    int width = w;
+
+    do {
+      uint8x16_t s0, s1, s2, s3, s4, s5, s6, s7;
+      load_u8_16x8(s, src_stride, &s0, &s1, &s2, &s3, &s4, &s5, &s6, &s7);
+
+      uint8x8_t d0 =
+          scale_4_to_1_filter8_8(s0, s1, s2, s3, permute_tbl, filter);
+      uint8x8_t d1 =
+          scale_4_to_1_filter8_8(s4, s5, s6, s7, permute_tbl, filter);
+
+      store_u8x2_strided_x4(d + 0 * dst_stride, dst_stride, d0);
+      store_u8x2_strided_x4(d + 4 * dst_stride, dst_stride, d1);
+
+      d += 2;
+      s += 8;
+      width -= 2;
+    } while (width > 0);
+
+    dst += 8 * dst_stride;
+    src += 8 * src_stride;
+    h -= 8;
+  } while (h > 0);
+}
+
+static inline void scale_plane_4_to_1_8tap(const uint8_t *src,
+                                           const int src_stride, uint8_t *dst,
+                                           const int dst_stride, const int w,
+                                           const int h,
+                                           const int16_t *const filter_ptr,
+                                           uint8_t *const im_block) {
+  assert(w > 0 && h > 0);
+  const int im_h = 4 * h + SUBPEL_TAPS - 2;
+  const int im_stride = (w + 1) & ~1;
+  // All filter values are even, halve them to fit in int8_t when applying
+  // horizontal filter and stay in 16-bit elements when applying vertical
+  // filter.
+  const int16x8_t filters = vshrq_n_s16(vld1q_s16(filter_ptr), 1);
+
+  const ptrdiff_t horiz_offset = SUBPEL_TAPS / 2 - 1;
+  const ptrdiff_t vert_offset = (SUBPEL_TAPS / 2 - 1) * src_stride;
+
+  scale_4_to_1_horiz_8tap(src - horiz_offset - vert_offset, src_stride, w, im_h,
+                          im_block, im_stride, filters);
+
+  // We can specialise the vertical filtering for 6-tap filters given that the
+  // EIGHTTAP_SMOOTH and EIGHTTAP_REGULAR filters are 0-padded.
+  scale_4_to_1_vert_6tap(im_block + im_stride, im_stride, w, h, dst, dst_stride,
+                         filters);
+}
+
 static inline bool has_normative_scaler_neon_dotprod(const int src_width,
                                                      const int src_height,
                                                      const int dst_width,
                                                      const int dst_height) {
-  return (2 * dst_width == src_width && 2 * dst_height == src_height);
+  return (2 * dst_width == src_width && 2 * dst_height == src_height) ||
+         (4 * dst_width == src_width && 4 * dst_height == src_height);
 }
 
 void av1_resize_and_extend_frame_neon_dotprod(const YV12_BUFFER_CONFIG *src,
@@ -183,6 +284,22 @@ void av1_resize_and_extend_frame_neon_dotprod(const YV12_BUFFER_CONFIG *src,
           (const InterpKernel *)av1_interp_filter_params_list[filter]
               .filter_ptr;
       scale_plane_2_to_1_8tap(src->buffers[i], src->strides[is_uv],
+                              dst->buffers[i], dst->strides[is_uv], dst_w,
+                              dst_h, interp_kernel[phase], temp_buffer);
+      free(temp_buffer);
+    } else if (4 * dst_w == src_w && 4 * dst_h == src_h) {
+      const int buffer_stride = (dst_y_w + 1) & ~1;
+      const int buffer_height = (4 * dst_y_h + SUBPEL_TAPS - 2 + 7) & ~7;
+      uint8_t *const temp_buffer =
+          (uint8_t *)malloc(buffer_stride * buffer_height);
+      if (!temp_buffer) {
+        malloc_failed = 1;
+        break;
+      }
+      const InterpKernel *interp_kernel =
+          (const InterpKernel *)av1_interp_filter_params_list[filter]
+              .filter_ptr;
+      scale_plane_4_to_1_8tap(src->buffers[i], src->strides[is_uv],
                               dst->buffers[i], dst->strides[is_uv], dst_w,
                               dst_h, interp_kernel[phase], temp_buffer);
       free(temp_buffer);
