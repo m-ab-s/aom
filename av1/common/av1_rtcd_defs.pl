241da46745d4861e0da96fefcd263cde45b3e981
James Zern <jzern@google.com>
2024-10-03 18:58:15 -0700

    rm aom_highbd_dist_wtd_comp_avg_upsampled_pred
    This function has not been used since:
    c05147803f use_jnt_comp_avg should be 0 in motion search
    when it was named aom_highbd_jnt_comp_avg_upsampled_pred.
    
    Bug: aomedia:3416
    Change-Id: I6be4558c7ca8591eed778ffd15f4c3ba07f82ca4

--- av1/common/av1_rtcd_defs.pl
+++ av1/common/av1_rtcd_defs.pl
@@ -285,12 +285,6 @@ if (aom_config("CONFIG_AV1_ENCODER") eq "yes") {
                                                             const MV *const mv, uint8_t *comp_pred8, const uint8_t *pred8, int width,
                                                             int height, int subpel_x_q3, int subpel_y_q3, const uint8_t *ref8, int ref_stride, int bd, int subpel_search";
     specialize qw/aom_highbd_comp_avg_upsampled_pred sse2 neon/;
-
-    add_proto qw/void aom_highbd_dist_wtd_comp_avg_upsampled_pred/, "MACROBLOCKD *xd, const struct AV1Common *const cm, int mi_row, int mi_col,
-                                                                const MV *const mv, uint8_t *comp_pred8, const uint8_t *pred8, int width,
-                                                                int height, int subpel_x_q3, int subpel_y_q3, const uint8_t *ref8,
-                                                                int ref_stride, int bd, const DIST_WTD_COMP_PARAMS *jcp_param, int subpel_search";
-    specialize qw/aom_highbd_dist_wtd_comp_avg_upsampled_pred sse2 neon/;
   }
 
   # the transform coefficients are held in 32-bit
