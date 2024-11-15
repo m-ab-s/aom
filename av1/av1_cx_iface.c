4903fe7782e8866f4593b80af11cf6086d83ebdb
Maryla <maryla@google.com>
2024-11-15 14:40:16 +0100

    Add 'is_layer_specific_obu' param to av1_write_obu_header
    Refactor to later allow making some metadata OBUs layer-specific.
    
    Bug: 377851082
    
    Change-Id: Ice49b9fa4ee3053d0eb93d6bec821dbac1bd153e

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -3507,7 +3507,8 @@ static aom_codec_err_t encoder_encode(aom_codec_alg_priv_t *ctx,
         obu_header_size = av1_write_obu_header(
             &ppi->level_params, &cpi->frame_header_count,
             OBU_TEMPORAL_DELIMITER,
-            ppi->seq_params.has_nonzero_operating_point_idc, 0, ctx->cx_data);
+            ppi->seq_params.has_nonzero_operating_point_idc,
+            /*is_layer_specific_obu=*/false, 0, ctx->cx_data);
         if (obu_header_size != 1) {
           aom_internal_error(&ppi->error, AOM_CODEC_ERROR, NULL);
         }
