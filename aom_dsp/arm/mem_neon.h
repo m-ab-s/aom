feba11b38dedaeae4f833cddacaf54e1aac00344
Salome Thirot <salome.thirot@arm.com>
2025-01-20 17:42:03 +0000

    Remove interleaving load/stores in cfl_predict_neon
    Interleaving is not necessary and interleaving load/stores can be very
    slow. Write new helper functions for old compiler versions that don't
    support these _x2/_x4 variants.
    
    Change-Id: I9d865acc68528e3ea192d56e2cceab5bad87d14b

--- aom_dsp/arm/mem_neon.h
+++ aom_dsp/arm/mem_neon.h
@@ -55,10 +55,50 @@ static inline uint16x8x4_t vld1q_u16_x4(const uint16_t *ptr) {
   return res;
 }
 
+static inline int16x8x2_t vld1q_s16_x2(const int16_t *ptr) {
+  int16x8x2_t res = { { vld1q_s16(ptr + 0 * 8), vld1q_s16(ptr + 1 * 8) } };
+  return res;
+}
+
+static inline int16x8x4_t vld1q_s16_x4(const int16_t *ptr) {
+  int16x8x4_t res = { { vld1q_s16(ptr + 0 * 8), vld1q_s16(ptr + 1 * 8),
+                        vld1q_s16(ptr + 2 * 8), vld1q_s16(ptr + 3 * 8) } };
+  return res;
+}
+
+static inline void vst1_u8_x2(uint8_t *ptr, uint8x8x2_t a) {
+  vst1_u8(ptr + 0 * 8, a.val[0]);
+  vst1_u8(ptr + 1 * 8, a.val[1]);
+}
+
+static inline void vst1_u8_x4(uint8_t *ptr, uint8x8x4_t a) {
+  vst1_u8(ptr + 0 * 8, a.val[0]);
+  vst1_u8(ptr + 1 * 8, a.val[1]);
+  vst1_u8(ptr + 2 * 8, a.val[2]);
+  vst1_u8(ptr + 3 * 8, a.val[3]);
+}
+
+static inline void vst1q_u16_x2(uint16_t *ptr, uint16x8x2_t a) {
+  vst1q_u16(ptr + 0 * 8, a.val[0]);
+  vst1q_u16(ptr + 1 * 8, a.val[1]);
+}
+
+static inline void vst1q_u16_x4(uint16_t *ptr, uint16x8x4_t a) {
+  vst1q_u16(ptr + 0 * 8, a.val[0]);
+  vst1q_u16(ptr + 1 * 8, a.val[1]);
+  vst1q_u16(ptr + 2 * 8, a.val[2]);
+  vst1q_u16(ptr + 3 * 8, a.val[3]);
+}
+
 #elif defined(__GNUC__) && !defined(__clang__)  // GCC 64-bit.
 #if __GNUC__ < 8
 static inline uint8x16x2_t vld1q_u8_x2(const uint8_t *ptr) {
   uint8x16x2_t res = { { vld1q_u8(ptr + 0 * 16), vld1q_u8(ptr + 1 * 16) } };
+  return res;
+}
+
+static inline int16x8x2_t vld1q_s16_x2(const int16_t *ptr) {
+  int16x8x2_t res = { { vld1q_s16(ptr + 0 * 8), vld1q_s16(ptr + 1 * 8) } };
   return res;
 }
 #endif  // __GNUC__ < 8
@@ -71,12 +111,29 @@ static inline uint8x16x3_t vld1q_u8_x3(const uint8_t *ptr) {
 }
 #endif  // __GNUC__ < 9
 
-// vld1q_u16_x4 is defined from GCC 8.5.0 and onwards.
 #if ((__GNUC__ << 8) | __GNUC_MINOR__) < 0x805
 static inline uint16x8x4_t vld1q_u16_x4(const uint16_t *ptr) {
   uint16x8x4_t res = { { vld1q_u16(ptr + 0 * 8), vld1q_u16(ptr + 1 * 8),
                          vld1q_u16(ptr + 2 * 8), vld1q_u16(ptr + 3 * 8) } };
   return res;
+}
+
+static inline int16x8x4_t vld1q_s16_x4(const int16_t *ptr) {
+  int16x8x4_t res = { { vld1q_s16(ptr + 0 * 8), vld1q_s16(ptr + 1 * 8),
+                        vld1q_s16(ptr + 2 * 8), vld1q_s16(ptr + 3 * 8) } };
+  return res;
+}
+
+static inline void vst1_u8_x2(uint8_t *ptr, uint8x8x2_t a) {
+  vst1_u8(ptr + 0 * 8, a.val[0]);
+  vst1_u8(ptr + 1 * 8, a.val[1]);
+}
+
+static inline void vst1_u8_x4(uint8_t *ptr, uint8x8x4_t a) {
+  vst1_u8(ptr + 0 * 8, a.val[0]);
+  vst1_u8(ptr + 1 * 8, a.val[1]);
+  vst1_u8(ptr + 2 * 8, a.val[2]);
+  vst1_u8(ptr + 3 * 8, a.val[3]);
 }
 #endif  // ((__GNUC__ << 8) | __GNUC_MINOR__) < 0x805
 #endif  // defined(__GNUC__) && !defined(__clang__)
