e4cf34956df42224b48c77c9b4fbfafd9e7fa9dd
Marco Paniconi <marpan@google.com>
2025-04-09 14:29:23 -0700

    rtc-svc: Fix to the layer_id under dynamic change.
    This is a fix to the revert issue here:
    https://chromium-review.googlesource.com/c/chromium/src/+/6442206
    
    The failing SVC test changes the svc->number_temporal_layers
    dynamically from 2 to 1, and when it switches to 1 it does
    not use the ctrl_set_layer_id to set temporal_layer_id to 0.
    Since the switch happened on a TL1 frame, the new temporal_layer_id
    in the encoder was kept to 1, even though the number_temporal_layers
    is 1.
    
    This was causing the assert failure in libaom:
    assert(IMPLIES(!has_nonzero_operating_point_idc, obu_extension == 0));
    
    The fix is to clamp the spatial/temporal_layer_id to proper values
    on dynamic change of layers.
    
    Change-Id: I36058fd309cbbaee18d59708606a299408983f34

--- av1/av1_cx_iface.c
+++ av1/av1_cx_iface.c
@@ -3896,6 +3896,23 @@ static aom_codec_err_t ctrl_set_svc_params(aom_codec_alg_priv_t *ctx,
         ppi->number_spatial_layers * ppi->number_temporal_layers - 1;
     ctx->next_frame_flags |= AOM_EFLAG_FORCE_KF;
     av1_set_svc_seq_params(ppi);
+    // Check for valid values for the spatial/temporal_layer_id here, since
+    // there has been a dynamic change in the number_spatial/temporal_layers,
+    // and if the ctrl_set_layer_id is not used after this call, the
+    // previous (last_encoded) values of spatial/temporal_layer_id will be used,
+    // which may be invalid.
+    cpi->svc.spatial_layer_id = AOMMAX(
+        0,
+        AOMMIN(cpi->svc.spatial_layer_id, cpi->svc.number_spatial_layers - 1));
+    cpi->svc.temporal_layer_id =
+        AOMMAX(0, AOMMIN(cpi->svc.temporal_layer_id,
+                         cpi->svc.number_temporal_layers - 1));
+    cpi->common.spatial_layer_id =
+        AOMMAX(0, AOMMIN(cpi->common.spatial_layer_id,
+                         cpi->svc.number_spatial_layers - 1));
+    cpi->common.temporal_layer_id =
+        AOMMAX(0, AOMMIN(cpi->common.temporal_layer_id,
+                         cpi->svc.number_temporal_layers - 1));
   }
 
   if (ppi->number_spatial_layers > 1 || ppi->number_temporal_layers > 1) {
