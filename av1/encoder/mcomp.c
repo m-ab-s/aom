007315bb2ea13b2323ae7487a9e17b5f5f9b3fdc
Jingning Han <jingning@google.com>
2025-05-15 23:30:19 -0700

    Resolve assertion failure
    Resolve assertion failure at av1_is_subpelmv_in_range() check.
    
    BUG=b/418084084
    
    Change-Id: I548f78979fd305fe272c89008cc513a9da96f397

--- av1/encoder/mcomp.c
+++ av1/encoder/mcomp.c
@@ -109,6 +109,10 @@ void av1_make_default_fullpel_ms_params(
         cpi->common.height - mi_size_high[bsize] * MI_SIZE - top_margin + 16;
     int right_margin =
         cpi->common.width - mi_size_wide[bsize] * MI_SIZE - left_margin + 16;
+
+    bottom_margin = AOMMAX(bottom_margin, -top_margin);
+    right_margin = AOMMAX(right_margin, -left_margin);
+
     FullMvLimits *mv_limits = &ms_params->mv_limits;
     mv_limits->row_min = AOMMAX(mv_limits->row_min, -top_margin);
     mv_limits->row_max = AOMMIN(mv_limits->row_max, bottom_margin);
@@ -199,6 +203,10 @@ void av1_make_default_subpel_ms_params(SUBPEL_MOTION_SEARCH_PARAMS *ms_params,
     int right_margin =
         GET_MV_SUBPEL(cpi->common.width - mi_size_wide[bsize] * MI_SIZE -
                       x->e_mbd.mi_col * MI_SIZE + 8);
+
+    bottom_margin = AOMMAX(bottom_margin, -top_margin);
+    right_margin = AOMMAX(right_margin, -left_margin);
+
     SubpelMvLimits *mv_limits = &ms_params->mv_limits;
     mv_limits->row_min = AOMMAX(mv_limits->row_min, -top_margin);
     mv_limits->row_max = AOMMIN(mv_limits->row_max, bottom_margin);
