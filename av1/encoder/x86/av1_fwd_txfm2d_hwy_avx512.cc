607793dd5d2e9af03f0ed9f78552287380f99120
Jeremy Dorfman <jdorfman@google.com>
2025-06-18 10:21:12 -0400

    fwd_txfm2d: switch Highway to AVX3_DL
    libaom's AVX512 guarantees AVX3_DL support.
    
    Change-Id: If3b80b27a57b39bf10301c1406e8c51665e9cc3d

--- av1/encoder/x86/av1_fwd_txfm2d_hwy_avx512.cc
+++ av1/encoder/x86/av1_fwd_txfm2d_hwy_avx512.cc
@@ -9,7 +9,7 @@
  * PATENTS file, you can obtain it at www.aomedia.org/license/patent.
  */
 
-#define HWY_BASELINE_TARGETS HWY_AVX3
+#define HWY_BASELINE_TARGETS HWY_AVX3_DL
 #define HWY_BROKEN_32BIT 0
 
 #include "av1/encoder/av1_fwd_txfm2d_hwy.h"
