5241dd3af6f8b9670812f0f3efdeb1719d2c70e2
Julio Barba <juliobbv@gmail.com>
2025-07-17 17:30:27 -0400

    Introduce adaptive sharpness
    This new setting modulates sharpness based on frame QP, which helps
    mitigate blocking artifacts in the low to medium quality range.
    
    This can be enabled by parameter `--enable-adaptive-sharpness=1`, or
    by using codec control AV1E_SET_ENABLE_ADAPTIVE_SHARPNESS.
    
    Enabling adaptive sharpness comes with a very small (but measurable)
    decrease of SSIMULACRA2 scores (Daala's subset1), so only enable by
    default for AOM_TUNE_IQ.
    
    Bug: aomedia:401234702
    Change-Id: I14f5bf0aab4e56f23704f28c502851bd1a95424c

--- aom/aomcx.h
+++ aom/aomcx.h
@@ -1600,6 +1600,15 @@ enum aome_enc_control_id {
    */
   AV1E_SET_SCREEN_CONTENT_DETECTION_MODE = 171,
 
+  /*!\brief Codec control to enable adaptive sharpness, which modulates
+   * sharpness based on frame QP. This helps mitigate blocking artifacts in the
+   * low to medium quality range. unsigned int parameter.
+   *
+   * - 0 = disable (default)
+   * - 1 = enable
+   */
+  AV1E_SET_ENABLE_ADAPTIVE_SHARPNESS = 172,
+
   // Any new encoder control IDs should be added above.
   // Maximum allowed encoder control ID is 229.
   // No encoder control ID should be added below.
@@ -1709,6 +1718,7 @@ typedef enum {
  *   * --qm-min=2
  *   * --qm-max=10
  *   * --sharpness=7
+ *   * --enable-adaptive-sharpness=1 (AOM_TUNE_IQ only)
  *   * --dist-metric=qm-psnr
  *   * --enable-cdef=3
  *   * --enable-chroma-deltaq=1
@@ -2336,6 +2346,9 @@ AOM_CTRL_USE_TYPE(AV1E_SET_ENABLE_LOW_COMPLEXITY_DECODE, unsigned int)
 AOM_CTRL_USE_TYPE(AV1E_SET_SCREEN_CONTENT_DETECTION_MODE,
                   int) /* aom_screen_detection_mode */
 #define AOM_CTRL_SET_SCREEN_CONTENT_DETECTION_MODE
+
+AOM_CTRL_USE_TYPE(AV1E_SET_ENABLE_ADAPTIVE_SHARPNESS, unsigned int)
+#define AOM_CTRL_AV1E_SET_ENABLE_ADAPTIVE_SHARPNESS
 
 /*!\endcond */
 /*! @} - end defgroup aom_encoder */
