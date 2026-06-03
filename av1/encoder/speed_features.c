608ad413b1e42a0eb7a93f4723c96a31fbc29f26
Mudassir Galaganath <mudassir.galaganath@ittiam.com>
2026-06-03 17:15:07 +0530

    lc-dec: Extend sf dual_sgr_penalty_level for VOD test sets
    This patch enables the speed feature 'dual_sgr_penalty_level' for
    VOD test sets in lc decode mode.
    
    Performance results of encoder and decoder for the streams
    generated with low complexity decode enabled.
    
          Decode Time                   BD-Rate Loss(%)
    cpu  Reduction(%)  avg.psnr  ssim    vmaf    vmaf_neg  uvq_1p5
     1     1.908       0.1250   0.1928  -0.1177  -0.0672   0.9588
     2     1.455       0.0799   0.1085  -0.0695  -0.0532   0.5862
     3     1.189       0.0905   0.0787  -0.0960  -0.0520   0.6495
    
    STATS_CHANGED for low complexity decode, hdres
    
    Change-Id: I02ab642c69b44119340297ab5c1ed4e7e756400b

--- av1/encoder/speed_features.c
+++ av1/encoder/speed_features.c
@@ -670,6 +670,7 @@ static void set_good_speed_features_lc_dec_framesize_dependent(
     sf->hl_sf.ref_frame_mvs_lvl = 1;
 
     sf->lpf_sf.adaptive_cdef_mode = 1;
+    sf->lpf_sf.dual_sgr_penalty_level = boosted ? 1 : 3;
     sf->lpf_sf.skip_loop_filter_using_filt_error =
         (update_type != OVERLAY_UPDATE && update_type != INTNL_OVERLAY_UPDATE &&
          cm->current_frame.pyramid_level > 1)
