998a3fcda4e4de068eebb5a1f766be8126e83661
James Zern <jzern@google.com>
2026-07-08 08:40:11 -0700

    av1_encode: disable do_border_pad
    This causes crashes in in Arm builds.
    
    Bug: 527078408, 527242002
    
    Change-Id: I6e4d8a7e096aff6908b30499bd063d97b6469a06

--- av1/encoder/encoder.c
+++ av1/encoder/encoder.c
@@ -4558,7 +4558,9 @@ int av1_encode(AV1_COMP *const cpi, uint8_t *const dest, size_t dest_size,
       cpi->oxcf.algo_cfg.enable_tpl_model && cpi->oxcf.q_cfg.aq_mode == NO_AQ &&
       !cpi->common.seg.enabled && !cpi->roi.enabled && !cpi->oxcf.sb_qp_sweep &&
       !cpi->use_ducky_encode && cpi->oxcf.algo_cfg.sharpness != 3) {
-    cpi->do_border_pad = true;
+    // TODO(issue 527242002, issue 527078408): set do_border_pad to true after
+    // fixing crashes in Arm builds.
+    cpi->do_border_pad = false;
   } else {
     cpi->do_border_pad = false;
   }
