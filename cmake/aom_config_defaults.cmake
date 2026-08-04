01fd4524390f5230b22e9449a79d5df5a1b76dc4
James Zern <jzern@google.com>
2026-08-04 12:56:52 -0700

    cmake: mark internal cache vars as advanced
    `CONFIG_OS_SUPPORT` and `CONFIG_SHARED` are marked this way in this
    change. `CONFIG_SHARED` is also labeled as an internal flag to avoid
    confusion with `BUILD_SHARED_LIBS`.
    
    Bug: 437872594
    Fixed: 437872594
    
    Change-Id: I4617c746e1776f41c719d24374b2221407f8ffeb

--- cmake/aom_config_defaults.cmake
+++ cmake/aom_config_defaults.cmake
@@ -96,6 +96,7 @@ set_aom_config_var(CONFIG_AV1_TEMPORAL_DENOISING 0
                    "Build with temporal denoising support.")
 set_aom_config_var(CONFIG_MULTITHREAD 1 "Multithread support.")
 set_aom_config_var(CONFIG_OS_SUPPORT 0 "Internal flag.")
+mark_as_advanced(CONFIG_OS_SUPPORT)
 set_aom_config_var(CONFIG_PIC 0 "Build with PIC enabled.")
 set_aom_config_var(CONFIG_QUANT_MATRIX 1
                    "Build with quantization matrices for AV1 encoder."
@@ -103,7 +104,8 @@ set_aom_config_var(CONFIG_QUANT_MATRIX 1
 set_aom_config_var(CONFIG_REALTIME_ONLY 0
                    "Build for RTC-only. See aomcx.h for all disabled features.")
 set_aom_config_var(CONFIG_RUNTIME_CPU_DETECT 1 "Runtime CPU detection support.")
-set_aom_config_var(CONFIG_SHARED 0 "Build shared libs.")
+set_aom_config_var(CONFIG_SHARED 0 "Internal flag.")
+mark_as_advanced(CONFIG_SHARED)
 set_aom_config_var(CONFIG_WEBM_IO 1 "Enables WebM support.")
 
 # Debugging flags.
